16fae5ed73f9924db3a0aa16e40947e90242b8a5
hpk <none@none>
2007-02-01 15:35:07 +0100

    [svn r37736] cosmetic docstring change
    --HG--
    branch : trunk

--- py/io/dupfile.py
+++ py/io/dupfile.py
@@ -4,8 +4,8 @@ import os
 def dupfile(f, mode=None, buffering=0, raising=False): 
     """ return a new open file object that's a duplicate of f
 
-        mode is duplicated if not given, buffering controls 
-        buffer size (defaulting to no buffering) and raising
+        mode is duplicated if not given, 'buffering' controls 
+        buffer size (defaulting to no buffering) and 'raising'
         defines whether an exception is raised when an incompatible
         file object is passed in (if raising is False, the file
         object itself will be returned)
