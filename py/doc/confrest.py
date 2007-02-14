74ad33da2822efed9a4e6182ede3e27b943cfcdb
guido <none@none>
2007-02-14 02:18:06 +0100

    [svn r38776] Removed duplicate links.
    --HG--
    branch : trunk

--- py/doc/confrest.py
+++ py/doc/confrest.py
@@ -44,8 +44,6 @@ class Page(object):
             self.a_apigenref("source", "source/index.html"),
             self.a_docref("contact", "contact.html"),
             self.a_docref("download", "download.html"),
-            html.a("contact", href="contact.html", class_="menu"),
-            html.a("download", href="download.html", class_="menu"),
         ]
         items2 = [items.pop(0)]
         sep = " "
