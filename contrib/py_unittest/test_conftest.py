eebb9d72575e7de701527a796832a5c17343546c
hpk <none@none>
2008-09-21 09:19:30 +0200

    [svn r58287] add a test for __version__ attr
    --HG--
    branch : trunk

--- contrib/py_unittest/test_conftest.py
+++ contrib/py_unittest/test_conftest.py
@@ -3,6 +3,10 @@ from py.__.test.outcome import Failed
 from py.__.test.testing import suptest
 conftestpath = py.magic.autopath().dirpath("conftest.py")
 
+def test_version():
+    mod = conftestpath.pyimport()
+    assert hasattr(mod, "__version__")
+
 class TestTestCaseInstance(suptest.InlineSession):
     def setup_method(self, method):
         super(TestTestCaseInstance, self).setup_method(method)
