aeca93f0b3127f4ec941c2ab2af4a64a439e9d87
pedronis <none@none>
2008-09-22 21:30:13 +0200

    [svn r58340] this seems to need to be this way, otherwise a py.lookup from PATH will be used or a failure will occur instead
    --HG--
    branch : trunk

--- py/cmdline/testing/test_cmdline.py
+++ py/cmdline/testing/test_cmdline.py
@@ -4,7 +4,7 @@ from py.__.test.testing.acceptance_test import AcceptBase
 class TestPyLookup(AcceptBase):
     def test_basic(self):
         p = self.makepyfile(hello="def x(): pass")
-        result = self.run("py.lookup", "pass")
+        result = self.runpybin("py.lookup", "pass")
         suptest.assert_lines_contain_lines(result.outlines, 
             ['%s:*def x(): pass' %(p.basename)]
         )
