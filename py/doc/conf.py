d53e3b3b67e76eecf6bec2b347f0e7fc81102465
hpk <none@none>
2009-04-02 13:16:41 +0200

    [svn r63529] remove idea here for a new "fs" namespace. 1.0.0 release announce draft some fixes and streamlines here and there.
    --HG--
    branch : trunk

--- py/doc/conf.py
+++ py/doc/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'basic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
