6104fa0142be2205827326f56fd761d7f92244b7
briandorsey <none@none>
2009-04-02 21:00:08 +0200

    [svn r63549] Attempted to create a theme based on the previous pylib pages. This initial version looks worse than either the old pages or the default sphinx output. Can revert back to the default theme by referting the change to conf.py Also added a new make target to force all html to be rebuilt. Sphinx doesn't detect changes to stylesheets in themes.
    --HG--
    branch : trunk

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'pylib'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +100,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_templates']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
