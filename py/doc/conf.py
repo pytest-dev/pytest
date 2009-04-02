1207d0d2deb87d7000db2b661efc9f4de2902e84
hpk <none@none>
2009-04-02 10:08:38 +0200

    [svn r63522] ReST fixes, Copyright fixes
    --HG--
    branch : trunk

--- py/doc/conf.py
+++ py/doc/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'py lib'
-copyright = u'2009, Holger Krekel'
+copyright = u'2009, Holger Krekel and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
