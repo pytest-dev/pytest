18a0dd8d091555a07645f0286cc8ba3250533974
hpk <none@none>
2009-04-02 15:43:26 +0200

    [svn r63542] moving pygreen to greenlet contrib dir
    --HG--
    branch : trunk

--- contrib/pygreen/conftest.py
+++
@@ -1,8 +0,0 @@
-import py, os
-
-class Directory(py.test.collect.Directory):
-    def collect(self): 
-        if os.name == 'nt':
-            py.test.skip("Cannot test green layer on windows")
-        else:
-            return super(Directory, self).collect()
