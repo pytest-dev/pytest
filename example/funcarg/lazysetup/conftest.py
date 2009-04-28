44abdc9391a058f2709e1946e1892d04f0eec33c
holger krekel <holger@merlinux.eu>
2009-04-28 19:49:48 +0200

    add an example for lazy "per-directory" setup.
    --HG--
    branch : trunk

---
+++ example/funcarg/lazysetup/conftest.py
@@ -0,0 +1,22 @@
+
+class ConftestPlugin:
+    def pytest_configure(self, config):
+        self._setup = None
+
+    def pytest_funcarg__setup(self, request):
+        if self._setup is None:
+            self._setup = LazySetup()
+        return self._setup
+
+    def pytest_unconfigure(self, config):
+        if self._setup is not None:
+            self._setup.finalize()
+
+class LazySetup:
+    def __init__(self):
+        import time
+        time.sleep(5)
+        self.timecostly = 1
+
+    def finalize(self):
+        del self.timecostly 
