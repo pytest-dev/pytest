b7fe3ee2b3bb5db09e30a59d0c8ed9ba894a8b93
holger krekel <holger@merlinux.eu>
2009-05-07 19:07:22 +0200

    fixes issue 6 by reverting back to issuing attributeerror
    --HG--
    branch : trunk

--- py/misc/std.py
+++ py/misc/std.py
@@ -2,11 +2,18 @@
 import sys
 
 class Std(object):
-    """ lazily import standard modules """
+    """ makes all standard python modules available as a lazily 
+        computed attribute. 
+    """ 
+
     def __init__(self):
         self.__dict__ = sys.modules
 
     def __getattr__(self, name):
-        return __import__(name)
+        try:
+            m = __import__(name)
+        except ImportError:
+            raise AttributeError("py.std: could not import %s" % name)
+        return m
 
 std = Std()
