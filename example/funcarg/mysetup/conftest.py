65a04bc3be5e452ed0336df30929dc4aa2cde079
holger krekel <holger@merlinux.eu>
2009-05-13 01:55:11 +0200

    simplify example code
    --HG--
    branch : trunk

--- example/funcarg/mysetup/conftest.py
+++ example/funcarg/mysetup/conftest.py
@@ -3,10 +3,8 @@ from myapp import MyApp
 
 class ConftestPlugin:
     def pytest_funcarg__mysetup(self, request):
-        return MySetup(request)
+        return MySetup()
 
 class MySetup:
-    def __init__(self, request):
-        self.config = request.config 
     def myapp(self):
         return MyApp()
