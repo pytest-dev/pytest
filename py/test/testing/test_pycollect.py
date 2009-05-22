e57543780f34f0035520ce442ea60a39238570ed
holger krekel <holger@merlinux.eu>
2009-05-22 23:57:08 +0200

    shift tests and remove superflous files
    --HG--
    branch : trunk

--- py/test/testing/test_pycollect.py
+++ py/test/testing/test_pycollect.py
@@ -268,6 +268,19 @@ class TestFunction:
         assert f5 != f5b
         assert not (f5 == f5b)
 
+    def test_pyfunc_call(self, testdir):
+        item = testdir.getitem("def test_func(): raise ValueError")
+        config = item.config
+        class MyPlugin1:
+            def pytest_pyfunc_call(self, pyfuncitem, *args, **kwargs):
+                raise ValueError
+        class MyPlugin2:
+            def pytest_pyfunc_call(self, pyfuncitem, *args, **kwargs):
+                return True
+        config.pluginmanager.register(MyPlugin1())
+        config.pluginmanager.register(MyPlugin2())
+        config.hook.pytest_pyfunc_call(pyfuncitem=item)
+
 class TestSorting:
     def test_check_equality(self, testdir):
         modcol = testdir.getmodulecol("""
