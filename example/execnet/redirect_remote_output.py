cd0972ede87a2ef73bf37862257987c82006a962
holger krekel <holger@merlinux.eu>
2009-07-10 13:44:33 +0200

    adding a new execnet example
    --HG--
    branch : 1.0.x

---
+++ example/execnet/redirect_remote_output.py
@@ -0,0 +1,31 @@
+"""
+redirect output from remote to a local function 
+showcasing features of the channel object:
+
+- sending a channel over a channel 
+- adapting a channel to a file object 
+- setting a callback for receiving channel data 
+
+"""
+
+import py
+
+gw = py.execnet.PopenGateway()
+
+outchan = gw.remote_exec("""
+    import sys
+    outchan = channel.gateway.newchannel()
+    sys.stdout = outchan.makefile("w")
+    channel.send(outchan) 
+""").receive()
+
+# note: callbacks execute in receiver thread! 
+def write(data):
+    print "received:", repr(data)
+outchan.setcallback(write)
+
+gw.remote_exec("""
+    print 'hello world'
+    print 'remote execution ends'
+""").waitclose()
+
