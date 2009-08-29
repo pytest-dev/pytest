8a6a3183ae2d1c98c45de208a46f7f4a47357ed0
Benjamin Peterson <benjamin@python.org>
2009-08-29 14:50:44 -0500

    guard against tests trying to import this
    --HG--
    branch : trunk

--- py/_testing/check_compile.py
+++ py/_testing/check_compile.py
@@ -1,10 +1,13 @@
 import sys
 
-fn = sys.argv[1]
-print("Testing %s" % (fn,))
-fp = open(fn, "rb")
-try:
-    source = fp.read()
-finally:
-    fp.close()
-compile(source, fn, "exec")
+def main(fn):
+    print("Testing %s" % (fn,))
+    fp = open(fn, "rb")
+    try:
+        source = fp.read()
+    finally:
+        fp.close()
+    compile(source, fn, "exec")
+
+if __name__ == "__main__":
+    main(sys.argv[1])
