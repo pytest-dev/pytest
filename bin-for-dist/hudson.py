d28838bbb634590949a1e2a8b3f6f722ef8fd8d2
holger krekel <holger@merlinux.eu>
2009-12-25 00:31:51 +0100

    try to fix windows path issue
    --HG--
    branch : trunk

--- bin-for-dist/hudson.py
+++ bin-for-dist/hudson.py
@@ -1,8 +1,6 @@
 import os, sys, subprocess, urllib
 
 BUILDNAME=os.environ.get('BUILD_NUMBER', "1")
-BIN=os.path.abspath(os.path.join(BUILDNAME, 'bin'))
-PYTHON=os.path.join(BIN, 'python')
 
 def call(*args):
     ret = subprocess.call(list(args))
@@ -14,6 +12,12 @@ def bincall(*args):
     call(*args)
 
 call("virtualenv", os.path.abspath(BUILDNAME), '--no-site-packages')
+BIN=os.path.abspath(os.path.join(BUILDNAME, 'bin'))
+if not os.path.exists(BIN):
+    BIN=os.path.abspath(os.path.join(BUILDNAME, 'Scripts'))
+    assert os.path.exists(BIN)
+    
+PYTHON=os.path.join(BIN, 'python')
 bincall("python", "setup.py", "develop", "-q")
 bincall("pip", "install", "-r", "testing/pip-reqs1.txt", 
                "-q", "--download-cache=download")
