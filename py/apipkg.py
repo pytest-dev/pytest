018254a907934201ba7cef3ad4c9da0b9ae522fb
holger krekel <holger@merlinux.eu>
2010-01-03 14:33:10 +0100

    update issues, version numbers
    --HG--
    branch : trunk

--- py/apipkg.py
+++ py/apipkg.py
@@ -8,7 +8,7 @@ see http://pypi.python.org/pypi/apipkg
 import sys
 from types import ModuleType
 
-__version__ = "1.0b3"
+__version__ = "1.0b4"
 
 def initpkg(pkgname, exportdefs):
     """ initialize given package from the export definitions. """
