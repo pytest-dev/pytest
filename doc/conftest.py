17bd8754449e14558eb47c6156da318bfc4300c7
holger krekel <holger@merlinux.eu>
2010-01-21 23:24:33 +0100

    fail doc generation if pygments is not installed
    --HG--
    branch : trunk

--- doc/conftest.py
+++ doc/conftest.py
@@ -3,3 +3,7 @@ import py
 #py.test.importorskip("pygments")
 pytest_plugins = ['pytest_restdoc']
 collect_ignore = ['test/attic.txt']
+
+def pytest_runtest_setup(item):
+    if item.fspath.ext == ".txt":
+        import pygments # for raising an error
