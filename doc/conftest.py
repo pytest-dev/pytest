d994c51ccd6c391ac7dd4c5932dabf6e20d78948
Benjamin Peterson <benjamin@python.org>
2010-03-09 16:18:27 -0600

    actually skip doc tests if pygments is not available
    --HG--
    branch : trunk

--- doc/conftest.py
+++ doc/conftest.py
@@ -1,9 +1,8 @@
 #XXX make work: excludedirs = ['_build']
 import py
-#py.test.importorskip("pygments")
 pytest_plugins = ['pytest_restdoc']
 collect_ignore = ['test/attic.txt']
 
 def pytest_runtest_setup(item):
     if item.fspath.ext == ".txt":
-        import pygments # for raising an error
+        py.test.importorskip("pygments") # for raising an error
