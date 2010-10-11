a82a6bb0588b86f1f07779295c16a6bb6a651b1c
holger krekel <holger@merlinux.eu>
2010-10-11 13:38:16 +0200

    regen docs, add a specific test_collectonly.py example
    --HG--
    branch : trunk

---
+++ doc/example/test_collectonly.py
@@ -0,0 +1,11 @@
+
+# run this with $ py.test --collectonly test_collectonly.py
+#
+def test_function():
+    pass
+
+class TestClass:
+    def test_method():
+        pass
+    def test_anothermethod():
+        pass
