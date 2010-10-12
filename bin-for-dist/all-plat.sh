22282eedb9cba974d3586301b0f76975b1628b71
holger krekel <holger@merlinux.eu>
2010-10-12 21:39:37 +0200

    remove superflous directory
    --HG--
    branch : trunk

--- bin-for-dist/all-plat.sh
+++
@@ -1,6 +0,0 @@
-
-        
-py.test --dist=each $* \
-    --tx 'socket=192.168.1.106:8888'
-    #--tx 'popen//python=python2.6' \
-    #--tx 'ssh=noco//python=/usr/local/bin/python2.4//chdir=/tmp/pytest-python2.4' \
