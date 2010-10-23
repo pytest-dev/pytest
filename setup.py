6cddd7e793aac49962c52a0b10c152a938f02e14
holger krekel <holger@merlinux.eu>
2010-10-23 15:42:53 +0200

    bump version
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def main():
         name='pytest',
         description='py.test: simple powerful testing with Python',
         long_description = long_description,
-        version= '2.0.0.dev3',
+        version='2.0.0.dev5',
         url='http://pytest.org',
         license='MIT license',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
@@ -67,4 +67,3 @@ def make_entry_points():
 
 if __name__ == '__main__':
     main()
-
