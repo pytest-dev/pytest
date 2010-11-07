3a53d86988fb6e5ef9918d579c35ea39a3f1eb9a
holger krekel <holger@merlinux.eu>
2010-11-07 16:26:44 +0100

    bump version

--- pytest/__init__.py
+++ pytest/__init__.py
@@ -5,7 +5,7 @@ see http://pytest.org for documentation and details
 
 (c) Holger Krekel and others, 2004-2010
 """
-__version__ = '2.0.0.dev23'
+__version__ = '2.0.0.dev24'
 
 __all__ = ['config', 'cmdline']
 
