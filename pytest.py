93a436542c69ea27cd1799266bb49f3f6d4fe7a2
holger krekel <holger@merlinux.eu>
2010-11-17 18:27:07 +0100

    bump version number

--- pytest.py
+++ pytest.py
@@ -5,7 +5,7 @@ see http://pytest.org for documentation and details
 
 (c) Holger Krekel and others, 2004-2010
 """
-__version__ = '2.0.0.dev29'
+__version__ = '2.0.0.dev30'
 __all__ = ['main']
 
 from _pytest.core import main, UsageError, _preloadplugins
