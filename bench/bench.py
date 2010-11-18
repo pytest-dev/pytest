5a2295ada518fbfb597f07c76d773385cc46171e
holger krekel <holger@merlinux.eu>
2010-11-18 15:19:20 +0100

    fix bare "py.test" runs without a directory by not defaulting to --doctest-modules which will virtually import everything

--- bench/bench.py
+++ bench/bench.py
@@ -1,9 +1,10 @@
 
-import cProfile
-import py
-import pstats
-stats = cProfile.run('py.test.cmdline.main(["empty.py", ])', 'prof')
-p = pstats.Stats("prof")
-p.strip_dirs()
-p.sort_stats('cumulative')
-print p.print_stats(30)
+if __name__ == '__main__':
+    import cProfile
+    import py
+    import pstats
+    stats = cProfile.run('py.test.cmdline.main(["empty.py", ])', 'prof')
+    p = pstats.Stats("prof")
+    p.strip_dirs()
+    p.sort_stats('cumulative')
+    print p.print_stats(30)
