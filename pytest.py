11e8e5570ebc7ddbbf92da79b4b4e1ea361a02c3
holger krekel <holger@merlinux.eu>
2010-11-23 17:21:34 +0100

    depend on py, not pylib distro

--- pytest.py
+++ pytest.py
@@ -5,7 +5,7 @@ see http://pytest.org for documentation and details
 
 (c) Holger Krekel and others, 2004-2010
 """
-__version__ = '2.0.0.dev33'
+__version__ = '2.0.0.dev34'
 __all__ = ['main']
 
 from _pytest.core import main, UsageError, _preloadplugins
