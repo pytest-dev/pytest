6ebd5f29004849907374391db43daf23b6c31fc6
holger krekel <holger@merlinux.eu>
2010-11-24 00:23:22 +0100

    improve docs

--- doc/conf.py
+++ doc/conf.py
@@ -135,6 +135,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {'index': 'indexsidebar.html'}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
