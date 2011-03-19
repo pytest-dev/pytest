b7ba4d4e70c3354fb8c6d006fd0e3f8165433717
holger krekel <holger@merlinux.eu>
2011-03-19 20:13:04 +0100

    shift version string to _pytest directory

--- pytest.py
+++ pytest.py
@@ -1,11 +1,11 @@
 """
 unit and functional testing with Python.
 """
-__version__ = '2.0.3.dev3'
 __all__ = ['main']
 
 from _pytest.core import main, UsageError, _preloadplugins
 from _pytest import core as cmdline
+from _pytest import __version__
 
 if __name__ == '__main__': # if run as a script or by 'python -m pytest'
     raise SystemExit(main())
