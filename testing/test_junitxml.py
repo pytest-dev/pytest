5498fe960f4b2b026a63434c8e8a3503a1ef4931
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-05-28 19:00:23 +0200

    add another normpath in the junitxml tests

--- testing/test_junitxml.py
+++ testing/test_junitxml.py
@@ -367,7 +367,7 @@ def test_invalid_xml_escape(testdir):
 def test_logxml_path_expansion():
     from _pytest.junitxml import LogXML
 
-    home_tilde = os.path.expanduser('~/test.xml')
+    home_tilde = os.path.normpath(os.path.expanduser('~/test.xml'))
     # this is here for when $HOME is not set correct
     home_var = os.path.normpath(os.path.expandvars('$HOME/test.xml'))
 
