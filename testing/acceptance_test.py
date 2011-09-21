eaec527a608dd06a9a4e2f84a4c5d44c8ff2a838
holger krekel <holger@merlinux.eu>
2011-09-21 06:21:48 +0200

    relax error string matching

--- testing/acceptance_test.py
+++ testing/acceptance_test.py
@@ -411,7 +411,7 @@ class TestInvocationVariants:
         result = testdir.runpytest("--pyargs", "tpkg.test_hello")
         assert result.ret != 0
         result.stderr.fnmatch_lines([
-            "*file*not*found*test_hello*",
+            "*not*found*test_hello*",
         ])
 
     def test_cmdline_python_package_not_exists(self, testdir):
