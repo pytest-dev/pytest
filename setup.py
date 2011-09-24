922a283f9982a053fc2f2acaa80930e492233b07
holger krekel <holger@merlinux.eu>
2011-09-24 14:13:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def main():
         name='pytest',
         description='py.test: simple powerful testing with Python',
         long_description = long_description,
-        version='2.1.2',
+        version='2.1.3dev1',
         url='http://pytest.org',
         license='MIT license',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
