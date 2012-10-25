bcb8dc71d2a38bbb73efeba2452aabf09c43c5e5
holger krekel <holger@merlinux.eu>
2012-10-25 12:08:11 +0200

    fix issue208 and fix issue29 - avoid long pauses in traceback printing by using the new getstatementrange() code of the py lib which uses AST-parsing rather than the previous heuristic which had O(n^2) complexity (with n = len(sourcelines))
    - require new (in-dev) py version to

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def main():
         entry_points= make_entry_points(),
         cmdclass = {'test': PyTest},
         # the following should be enabled for release
-        install_requires=['py>=1.4.10'],
+        install_requires=['py>=1.4.11.dev6'],
         classifiers=['Development Status :: 6 - Mature',
                      'Intended Audience :: Developers',
                      'License :: OSI Approved :: MIT License',
