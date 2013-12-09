a4466342aede289875fbd0bbb40b510705887847
holger krekel <holger@merlinux.eu>
2013-12-09 08:14:58 +0100

    make bench.py accept an optional script name and add a slow "manyparam" test

--- bench/bench.py
+++ bench/bench.py
@@ -1,9 +1,11 @@
+import sys
 
 if __name__ == '__main__':
     import cProfile
     import py
     import pstats
-    stats = cProfile.run('py.test.cmdline.main(["skip.py", ])', 'prof')
+    script = sys.argv[1] if len(sys.argv) > 1 else "empty.py"
+    stats = cProfile.run('py.test.cmdline.main([%r])' % script, 'prof')
     p = pstats.Stats("prof")
     p.strip_dirs()
     p.sort_stats('cumulative')
