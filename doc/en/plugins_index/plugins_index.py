0559f11aa5f73939aee625e835847fc5ad99d426
Bruno Oliveira <nicoddemus@gmail.com>
2014-02-18 21:33:13 -0300

    Improved text on plugins_index
    - Removed "beta" status from it;
    - Added links to pytest-plugs app;
    
    --HG--
    branch : integrate-plugin-index-docs

--- doc/en/plugins_index/plugins_index.py
+++ doc/en/plugins_index/plugins_index.py
@@ -1,6 +1,13 @@
 """
 Script to generate the file `index.txt` with information about
-pytest plugins taken directly from a live PyPI server.
+pytest plugins taken directly from PyPI.
+
+Usage:
+    python plugins_index.py
+
+This command will update `index.txt` in the same directory found as this script.
+This should be issued before every major documentation release to obtain latest
+versions from PyPI.
 
 Also includes plugin compatibility between different python and pytest versions,
 obtained from http://pytest-plugs.herokuapp.com.
@@ -167,11 +174,9 @@ def generate_plugins_index_from_table(filename, headers, rows):
         return ' '.join(char * length for length in column_lengths)
 
     with open(filename, 'w') as f:
-        # write welcome
-        print('.. _plugins_index:', file=f)
-        print(file=f)
-        print('List of Third-Party Plugins', file=f)
-        print('===========================', file=f)
+        # header
+        header_text = HEADER.format(pytest_version=pytest.__version__)
+        print(header_text, file=f)
         print(file=f)
 
         # table
@@ -230,6 +235,22 @@ def main(argv):
     print()
     print('%s Updated.' % options.filename)
     return 0
+
+
+# header for the plugins_index page
+HEADER = '''.. _plugins_index:
+
+List of Third-Party Plugins
+===========================
+
+The table below contains a listing of plugins found in PyPI and
+their status when tested using py.test **{pytest_version}** and python 2.7 and
+3.3.
+
+A complete listing can also be found at
+`pytest-plugs <http://pytest-plugs.herokuapp.com/>`_, which contains tests
+status against other py.test releases.
+'''
 
 
 if __name__ == '__main__':
