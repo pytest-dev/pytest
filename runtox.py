fd059359cce96befeadd4a103853f84d5ffc41a3
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2014-04-05 08:51:00 +0200

    make runtox.py not import external modules unless run as a script
    Was not needed since the script actually does nothing unless run as a script.
    
    --HG--
    branch : jurko/tox_usage

--- runtox.py
+++ runtox.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-import subprocess
-import sys
-
 if __name__ == "__main__":
+    import subprocess
+    import sys
     subprocess.call([sys.executable, "-m", "tox",
                      "-i", "ALL=https://devpi.net/hpk/dev/",
                      "--develop"] + sys.argv[1:])
