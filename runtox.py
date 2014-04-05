c2c504797e25d546c7bc7f9e900bb990ec6ba5c1
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2014-04-05 08:49:30 +0200

    stylistic runtox.py code cleanup
    --HG--
    branch : jurko/tox_usage

--- runtox.py
+++ runtox.py
@@ -6,5 +6,4 @@ import sys
 if __name__ == "__main__":
     subprocess.call([sys.executable, "-m", "tox",
                      "-i", "ALL=https://devpi.net/hpk/dev/",
-                     "--develop",] + sys.argv[1:])
-
+                     "--develop"] + sys.argv[1:])
