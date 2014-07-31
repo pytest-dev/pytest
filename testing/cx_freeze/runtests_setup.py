3c649cf91d831bcc066ecc141975270fe7a46846
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-30 22:28:03 -0300

    guarding scripts with __main__ and doc changes
    tox-flakes environment tries to import the modules for
    checking, and that may fail because of its dependencies
    
    --HG--
    branch : cx_freeze-support

--- testing/cx_freeze/runtests_setup.py
+++ testing/cx_freeze/runtests_setup.py
@@ -1,14 +1,15 @@
 """
 Sample setup.py script that generates an executable with pytest runner embedded.
 """
-from cx_Freeze import setup, Executable
-import pytest
+if __name__ == '__main__':
+    from cx_Freeze import setup, Executable
+    import pytest
 
-setup(
-    name="runtests",
-    version="0.1",
-    description="exemple of how embedding py.test into an executable using cx_freeze",
-    executables=[Executable("runtests_script.py")],
-    options={"build_exe": {'includes': pytest.cx_freeze_support.includes()}},
-)
+    setup(
+        name="runtests",
+        version="0.1",
+        description="exemple of how embedding py.test into an executable using cx_freeze",
+        executables=[Executable("runtests_script.py")],
+        options={"build_exe": {'includes': pytest.cx_freeze_support.includes()}},
+    )
 
