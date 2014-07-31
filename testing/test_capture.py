b1f8038abfb6203d4bf08cefff56cf5c81b2c87f
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-31 19:52:08 -0300

    Minor fixed in test_capture
    This test failed only in py34. We don't import logging directly,
    but it seems that one of the standard modules that are now
    globally imported started including this on py34.
    
    Just removed the assert as it doesn't seem central to the
    test's objective.

--- testing/test_capture.py
+++ testing/test_capture.py
@@ -340,8 +340,6 @@ class TestLoggingInteraction:
         p = testdir.makepyfile("""
             import sys
             def test_something():
-                # pytest does not import logging
-                assert 'logging' not in sys.modules
                 import logging
                 logging.basicConfig()
                 logging.warn("hello432")
