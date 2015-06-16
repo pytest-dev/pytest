a0da7b977428ca17a0a5b5b9d6cb120e20ba339f
Bruno Oliveira <nicoddemus@gmail.com>
2015-06-16 20:42:49 -0300

    Mention issue 713 in the test's docstring

--- testing/test_doctest.py
+++ testing/test_doctest.py
@@ -354,6 +354,9 @@ class TestDoctests:
         reprec.assertoutcome(skipped=1, failed=1, passed=0)
 
     def test_junit_report_for_doctest(self, testdir):
+        """
+        #713: Fix --junit-xml option when used with --doctest-modules.
+        """
         p = testdir.makepyfile("""
             def foo():
                 '''
