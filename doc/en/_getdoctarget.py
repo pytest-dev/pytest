a20c6d072d70c535ed1f116fc04016c834ea9c14
Bruno Oliveira <nicoddemus@gmail.com>
2015-08-23 11:20:34 -0300

    Fix getdoctarget to ignore comment lines

--- doc/en/_getdoctarget.py
+++ doc/en/_getdoctarget.py
@@ -6,7 +6,7 @@ def get_version_string():
     fn = py.path.local(__file__).join("..", "..", "..",
                                       "_pytest", "__init__.py")
     for line in fn.readlines():
-        if "version" in line:
+        if "version" in line and not line.strip().startswith('#'):
             return eval(line.split("=")[-1])
 
 def get_minor_version_string():
