1216a27b44a2eeaae8f02dd2675596581d57246c
aselus-hub <alexeik@peernova.com>
2015-12-10 15:19:08 -0800

    added docstrign to inection collection test.

--- testing/python/collect.py
+++ testing/python/collect.py
@@ -1079,6 +1079,12 @@ def test_dont_collect_non_function_callable(testdir):
 
 
 def test_class_injection_does_not_break_collection(testdir):
+    """Tests whether injection during collection time will terminate testing.
+
+    In this case the error should not occur if the TestClass itself
+    is modified during collection time, and the original method list
+    is still used for collection.
+    """
     testdir.makeconftest("""
         from test_inject import TestClass
         def pytest_generate_tests(metafunc):
