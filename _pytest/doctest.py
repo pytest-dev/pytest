e43eaffd9335824b4dd2c9c6fdb38606a0ac325b
Bruno Oliveira <nicoddemus@gmail.com>
2016-01-25 23:30:53 -0200

    Remove unused import

--- _pytest/doctest.py
+++ _pytest/doctest.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 
 import traceback
 
-import py
 import pytest
 from _pytest._code.code import TerminalRepr, ReprFileLocation, ExceptionInfo
 from _pytest.python import FixtureRequest
