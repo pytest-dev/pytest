b2a312cb5e9f559bdd11576963e09852ce9905ef
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-02-03 12:01:03 +0300

    Add expected exceptions to 'DID NOT RAISE' msg

--- _pytest/python.py
+++ _pytest/python.py
@@ -1309,7 +1309,7 @@ def raises(expected_exception, *args, **kwargs):
             func(*args[1:], **kwargs)
         except expected_exception:
             return py.code.ExceptionInfo()
-    pytest.fail("DID NOT RAISE")
+    pytest.fail("DID NOT RAISE {0}".format(expected_exception))
 
 class RaisesContext(object):
     def __init__(self, expected_exception):
@@ -1768,7 +1768,6 @@ class FixtureLookupError(LookupError):
             # the last fixture raise an error, let's present
             # it at the requesting side
             stack = stack[:-1]
-                               
         for function in stack:
             fspath, lineno = getfslineno(function)
             try:
