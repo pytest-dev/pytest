b631fc0bc1ad720b408b84964994641a631d3a74
Ceridwen <ceridwenv@gmail.com>
2016-04-01 17:57:42 -0400

    Fix test_escaped_parametrized_names_xml

--- testing/test_junitxml.py
+++ testing/test_junitxml.py
@@ -617,7 +617,7 @@ def test_escaped_parametrized_names_xml(testdir):
     result, dom = runandparse(testdir)
     assert result.ret == 0
     node = dom.find_first_by_tag("testcase")
-    node.assert_attr(name="test_func[#x00]")
+    node.assert_attr(name="test_func[\\x00]")
 
 
 def test_double_colon_split_function_issue469(testdir):
