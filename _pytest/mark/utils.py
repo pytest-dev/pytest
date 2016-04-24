8ec6a530eb0b28e9f9539a1e90875c171468ef3b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2016-04-24 22:21:06 +0200

    marker plugin: restructure and simplify marker transfer

--- _pytest/mark/utils.py
+++ _pytest/mark/utils.py
@@ -1,4 +1,4 @@
-from .model import apply_mark
+from .model import apply_mark, MarkDecorator
 
 
 def _marked(func, mark):
@@ -13,19 +13,22 @@ def _marked(func, mark):
     return mark.args == func_mark.args and mark.kwargs == func_mark.kwargs
 
 
-def transfer_markers(funcobj, cls, mod):
-    # XXX this should rather be code in the mark plugin or the mark
-    # plugin should merge with the python plugin.
-    for holder in (cls, mod):
-        try:
-            pytestmark = holder.pytestmark
-        except AttributeError:
-            continue
-        if isinstance(pytestmark, list):
-            for mark in pytestmark:
-
-                if not _marked(funcobj, mark):
-                    apply_mark(mark=mark, obj=funcobj)
+def get_marks(obj):
+    try:
+        maybe_mark = obj.pytestmark
+    except AttributeError:
+        return []
+    else:
+        if isinstance(maybe_mark, list):
+            return maybe_mark
+        elif isinstance(maybe_mark, MarkDecorator):
+            return [maybe_mark.mark]
         else:
-            if not _marked(funcobj, pytestmark):
-                apply_mark(mark=pytestmark, obj=funcobj)
+            raise TypeError('%r is not a mark' % (maybe_mark,))
+
+
+def transfer_markers(funcobj, cls, mod):
+    for holder in (cls, mod):
+        for mark in get_marks(holder):
+            if not _marked(funcobj, mark):
+                apply_mark(mark=mark, obj=funcobj)
