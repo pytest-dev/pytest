05aba8b4cf21104800b4c9bff2dc598072eaed71
RedBeardCode <s.farmbauer@red-beard-code.de>
2016-06-25 14:11:39 +0200

    Move the freezing function from genscript.py to a new module freeze_support.py

--- _pytest/config.py
+++ _pytest/config.py
@@ -65,7 +65,7 @@ _preinit = []
 default_plugins = (
      "mark main terminal runner python pdb unittest capture skipping "
      "tmpdir monkeypatch recwarn pastebin helpconfig nose assertion "
-     "junitxml resultlog doctest cacheprovider").split()
+     "junitxml resultlog doctest cacheprovider freeze_support").split()
 
 builtin_plugins = set(default_plugins)
 builtin_plugins.add("pytester")
