4d31ea83160331d8eb97de477bc3476d1a39653f
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-03-15 18:26:01 +0100

    add a comment explaining the modimport tests

--- testing/test_modimport.py
+++ testing/test_modimport.py
@@ -12,6 +12,10 @@ MODSET = [
 
 @pytest.mark.parametrize('modfile', MODSET, ids=lambda x: x.purebasename)
 def test_fileimport(modfile):
+    # this test ensures all internal packages can import
+    # without needing the pytest namespace being set
+    # this is critical for the initialization of xdist
+
     res = subprocess.call([
         sys.executable,
         '-c', 'import sys, py; py.path.local(sys.argv[1]).pyimport()',
