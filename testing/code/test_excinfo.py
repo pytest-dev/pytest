4d947077bbed4862d4e0a055ce967ba490be96af
Bruno Oliveira <nicoddemus@gmail.com>
2017-03-16 23:07:03 -0300

    Fix test in py26 that expected a floor division error message

--- testing/code/test_excinfo.py
+++ testing/code/test_excinfo.py
@@ -370,7 +370,7 @@ def test_codepath_Queue_example():
 
 def test_match_succeeds():
     with pytest.raises(ZeroDivisionError) as excinfo:
-        0 / 0
+        0 // 0
     excinfo.match(r'.*zero.*')
 
 def test_match_raises_error(testdir):
