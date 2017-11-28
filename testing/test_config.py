833f33fa0cfd16885f0f93fb288cc8d18b761d8c
Roland Puntaier <puntaier@gtec.at>
2017-11-28 17:38:49 +0100

    removed comments

--- testing/test_config.py
+++ testing/test_config.py
@@ -485,9 +485,6 @@ def test_plugin_preparse_prevents_setuptools_loading(testdir, monkeypatch):
     monkeypatch.setattr(pkg_resources, 'iter_entry_points', my_iter)
     config = testdir.parseconfig("-p", "no:mytestplugin")
     config.pluginmanager.import_plugin("mytestplugin")
-    #from _pytest.config import get_config
-    #pm = get_config().pluginmanager
-    #pm.import_plugin("mytestplugin")
     assert "mytestplugin" not in sys.modules
 
 
