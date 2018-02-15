3cbf0c8ec028e4fdba2e66327a2d4f01025804c4
Florian Bruhin <git@the-compiler.org>
2018-02-15 12:11:56 +0100

    Raise unexpected exceptions with pytest.raises() using match=

--- _pytest/python_api.py
+++ _pytest/python_api.py
@@ -617,6 +617,6 @@ class RaisesContext(object):
         suppress_exception = issubclass(self.excinfo.type, self.expected_exception)
         if sys.version_info[0] == 2 and suppress_exception:
             sys.exc_clear()
-        if self.match_expr:
+        if self.match_expr and suppress_exception:
             self.excinfo.match(self.match_expr)
         return suppress_exception
