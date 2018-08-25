3bb4d8a6c78bd58d462a7507697729dc454abdf2
Anthony Sottile <asottile@umich.edu>
2018-08-25 15:22:49 -0700

    Also record coverage for tests

--- src/_pytest/__init__.py
+++ src/_pytest/__init__.py
@@ -2,7 +2,7 @@ __all__ = ["__version__"]
 
 try:
     from ._version import version as __version__
-except ImportError:
+except ImportError:  # pragma: no cover
     # broken installation, we don't even try
     # unknown only works because we do poor mans version compare
     __version__ = "unknown"
