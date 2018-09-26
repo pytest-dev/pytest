6be2136f20c076b98b63233a701493c79acd7c6c
Zac-HD <zac.hatfield.dodds@gmail.com>
2018-09-26 22:02:14 +1000

    Ensure user_properties is a list

--- src/_pytest/reports.py
+++ src/_pytest/reports.py
@@ -110,7 +110,7 @@ class TestReport(BaseReport):
         when,
         sections=(),
         duration=0,
-        user_properties=(),
+        user_properties=None,
         **extra
     ):
         #: normalized collection node id
@@ -136,6 +136,11 @@ class TestReport(BaseReport):
 
         #: user properties is a list of tuples (name, value) that holds user
         #: defined properties of the test
+        if user_properties is None:
+            user_properties = []
+        elif not isinstance(user_properties, list):
+            # This used to be a tuple; so cast for backwards-compatibility
+            user_properties = list(user_properties)
         self.user_properties = user_properties
 
         #: list of pairs ``(str, str)`` of extra information which needs to
