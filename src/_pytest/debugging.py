57caa4e25e41d001eeb672e26750a5d3d64521f9
Daniel Hahler <git@thequod.de>
2018-10-24 23:43:41 +0200

    Fix flake8: os imported, but unused
    Likely due to b6fa4e24.

--- src/_pytest/debugging.py
+++ src/_pytest/debugging.py
@@ -1,7 +1,6 @@
 """ interactive debugging with PDB, the Python Debugger. """
 from __future__ import absolute_import, division, print_function
 
-import os
 import pdb
 import sys
 from doctest import UnexpectedException
