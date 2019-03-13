5c672c513c23c67f15de88ef77475ee7b33c7abd
Daniel Hahler <git@thequod.de>
2019-03-13 23:20:32 +0100

    type=args

--- src/_pytest/config/__init__.py
+++ src/_pytest/config/__init__.py
@@ -354,6 +354,7 @@ class PytestPluginManager(PluginManager):
             "load_entrypoint_plugins",
             help="only load specified plugins via entrypoint",
             default=notset,
+            type="args",
         )
 
     def pytest_configure(self, config):
@@ -816,7 +817,6 @@ class Config(object):
 
         load_entrypoint_plugins = self.getini("load_entrypoint_plugins")
         if load_entrypoint_plugins is not notset and not len(load_entrypoint_plugins):
-            assert isinstance(load_entrypoint_plugins, list)
             return
 
         if os.environ.get("PYTEST_DISABLE_PLUGIN_AUTOLOAD"):
