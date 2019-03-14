134b957bf42ef92f892ace04651c87ab6d09999f
Miro Hrončok <miro@hroncok.cz>
2019-03-14 12:15:05 +0100

    Remove deprecated Sphinx directive add_description_unit()
    Partial solution for https://github.com/pytest-dev/pytest/issues/4912

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -335,7 +335,7 @@ intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 def setup(app):
     # from sphinx.ext.autodoc import cut_lines
     # app.connect('autodoc-process-docstring', cut_lines(4, what=['module']))
-    app.add_description_unit(
+    app.add_object_type(
         "confval",
         "confval",
         objname="configuration value",
