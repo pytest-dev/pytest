403f556928e0000e8cd95df85040e750a0e75aa2
Gary Tyler <mail@garytyler.com>
2019-03-28 19:25:55 -0400

    Update docs for 'pytest_cmdline_parse' hook to note availability liminations

--- src/_pytest/hookspec.py
+++ src/_pytest/hookspec.py
@@ -99,8 +99,8 @@ def pytest_cmdline_parse(pluginmanager, args):
     Stops at first non-None result, see :ref:`firstresult`
 
     .. note::
-        This hook will not be called for ``conftest.py`` files, only for setuptools plugins or
-        modules which implement this hook and are early-loaded with the ``-p`` flag.
+        This hook will only be called for plugin classes passed to the ``plugins`` arg when using `pytest.main`_ to
+        perform an in-process test run.
 
     :param _pytest.config.PytestPluginManager pluginmanager: pytest plugin manager
     :param list[str] args: list of arguments passed on the command line
