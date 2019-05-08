7161f5b3726c569e184353059c6d382e9b5da53f
Sitaktif <sitaktif@gmail.com>
2019-05-08 15:56:28 +0100

    Require pluggy>=0.11
    Pluggy 0.10.0 introduced a new dependency `importlib-metadata` but that
    package cannot be imported when installed as an egg and that is causing
    problems for users relying on `setup.py` to install packages present in
    `test_require`. This change was quickly reverted in pluggy 0.11.0.
    
    See https://github.com/pytest-dev/pluggy/issues/205

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ INSTALL_REQUIRES = [
     'funcsigs>=1.0;python_version<"3.0"',
     'pathlib2>=2.2.0;python_version<"3.6"',
     'colorama;sys_platform=="win32"',
-    "pluggy>=0.9",
+    "pluggy>=0.11",
 ]
 
 
