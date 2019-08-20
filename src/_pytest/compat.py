43eab917a1d174808f1975364f64214f98b094a8
Ran Benita <ran@unusedvar.com>
2019-08-20 15:41:32 +0300

    Fix coverage

--- src/_pytest/compat.py
+++ src/_pytest/compat.py
@@ -350,7 +350,7 @@ class FuncargnamesCompatAttr:
         return self.fixturenames
 
 
-if sys.version_info < (3, 5, 2):
+if sys.version_info < (3, 5, 2):  # pragma: no cover
 
     def overload(f):  # noqa: F811
         return f
