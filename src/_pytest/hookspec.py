bde54d1bb9ac1fb304610fe0994f16136a5ef518
Ran Benita <ran@unusedvar.com>
2020-01-23 10:46:17 +0200

    Type annotate main.py and some parts related to collection

--- src/_pytest/hookspec.py
+++ src/_pytest/hookspec.py
@@ -1,13 +1,20 @@
 """ hook specifications for pytest plugins, invoked from main.py and builtin plugins.  """
 from typing import Any
+from typing import List
 from typing import Optional
+from typing import Union
 
+import py.path
 from pluggy import HookspecMarker
 
 from _pytest.compat import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from _pytest.main import Session
+    from _pytest.nodes import Collector
+    from _pytest.nodes import Item
+    from _pytest.python import Module
+    from _pytest.python import PyCollector
 
 
 hookspec = HookspecMarker("pytest")
@@ -215,7 +222,7 @@ def pytest_collect_directory(path, parent):
     """
 
 
-def pytest_collect_file(path, parent):
+def pytest_collect_file(path: py.path.local, parent) -> "Optional[Collector]":
     """ return collection Node or None for the given path. Any new node
     needs to have the specified ``parent`` as a parent.
 
@@ -255,7 +262,7 @@ def pytest_make_collect_report(collector):
 
 
 @hookspec(firstresult=True)
-def pytest_pycollect_makemodule(path, parent):
+def pytest_pycollect_makemodule(path: py.path.local, parent) -> "Optional[Module]":
     """ return a Module collector or None for the given path.
     This hook will be called for each matching test module path.
     The pytest_collect_file hook needs to be used if you want to
@@ -268,7 +275,9 @@ def pytest_pycollect_makemodule(path, parent):
 
 
 @hookspec(firstresult=True)
-def pytest_pycollect_makeitem(collector, name, obj):
+def pytest_pycollect_makeitem(
+    collector: "PyCollector", name: str, obj
+) -> "Union[None, Item, Collector, List[Union[Item, Collector]]]":
     """ return custom item/collector for a python object in a module, or None.
 
     Stops at first non-None result, see :ref:`firstresult` """
--- src/_pytest/reports.py
+++ src/_pytest/reports.py
@@ -20,7 +20,8 @@ from _pytest._code.code import ReprTraceback
 from _pytest._code.code import TerminalRepr
 from _pytest._io import TerminalWriter
 from _pytest.compat import TYPE_CHECKING
-from _pytest.nodes import Node
+from _pytest.nodes import Collector
+from _pytest.nodes import Item
 from _pytest.outcomes import skip
 from _pytest.pathlib import Path
 
@@ -315,7 +316,13 @@ class CollectReport(BaseReport):
     when = "collect"
 
     def __init__(
-        self, nodeid: str, outcome, longrepr, result: List[Node], sections=(), **extra
+        self,
+        nodeid: str,
+        outcome,
+        longrepr,
+        result: Optional[List[Union[Item, Collector]]],
+        sections=(),
+        **extra
     ) -> None:
         self.nodeid = nodeid
         self.outcome = outcome
