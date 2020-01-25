503e128cbe152b5fbf6a1fd32edffcc805e8ec8c
Daniel Hahler <git@thequod.de>
2020-01-25 15:05:11 +0100

    WIP: scripts/report-coverage.sh: codecov: pass -N

--- scripts/report-coverage.sh
+++ scripts/report-coverage.sh
@@ -15,4 +15,7 @@ python -m coverage xml
 python -m coverage report -m
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
 curl -S -L --connect-timeout 5 --retry 6 -s https://codecov.io/bash -o codecov-upload.sh
-bash codecov-upload.sh -Z -X fix -f coverage.xml "$@"
+
+base_branch="${GITHUB_BASE_REF:-${TRAVIS_BRANCH}}"
+base_commit=$(git merge-base FETCH_HEAD "origin/$base_branch")
+bash codecov-upload.sh -Z -X fix -f coverage.xml -N "$base_commit" "$@"
