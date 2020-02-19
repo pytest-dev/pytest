648460e9f7564693c56f431f053d8f6cbbb3ad98
Tor Colvin <Tor.Colvin@schrodinger.com>
2020-02-19 12:49:58 -0500

    Revert "Change assert to os.path.exists since too long will fail with pathlib"
    This reverts commit 849e3ade5fdd7d60c1ff0b79064ba379b9773ffc.

--- testing/test_pathlib.py
+++ testing/test_pathlib.py
@@ -4,7 +4,6 @@ import sys
 import py
 
 import pytest
-from _pytest.pathlib import create_long_path
 from _pytest.pathlib import fnmatch_ex
 from _pytest.pathlib import get_lock_path
 from _pytest.pathlib import maybe_delete_a_numbered_dir
@@ -102,5 +101,4 @@ def test_long_path_during_cleanup(tmp_path):
 
     lock_path = get_lock_path(path)
     maybe_delete_a_numbered_dir(path)
-    # is_file also fails on the long path
-    assert not os.path.exists(create_long_path(lock_path))
+    assert not lock_path.is_file()
