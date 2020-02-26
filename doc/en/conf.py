f77d606d4eeba38352bd3ea960f44b171fe06c1f
Daniel Hahler <git@thequod.de>
2020-02-27 00:34:18 +0100

    docs: generate index and add it to globaltoc sidebar (#6808)

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -208,7 +208,7 @@ html_sidebars = {
 html_domain_indices = True
 
 # If false, no index is generated.
-html_use_index = False
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 # html_split_index = False
