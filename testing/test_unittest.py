pytest-dev/pytest

commit 04f27d4eb469fb9c76fd2d100564a0f7d30028df

unittest: do not use TestCase.debug() with `--pdb`
Fixes https://github.com/pytest-dev/pytest/issues/5991
Fixes https://github.com/pytest-dev/pytest/issues/3823

Ref: https://github.com/pytest-dev/pytest-django/issues/772
Ref: https://github.com/pytest-dev/pytest/pull/1890
Ref: https://github.com/pytest-dev/pytest-django/pull/782

- inject wrapped testMethod

- adjust test_trial_error

- add test for `--trace` with unittests

src/_pytest/unittest.py
@@ -1,4 +1,5 @@
 """ discovery and running of std-library "unittest" style tests. """
+import functools
 import sys
 import traceback
 
@@ -107,6 +108,7 @@ class TestCaseFunction(Function):
     nofuncargs = True
     _excinfo = None
     _testcase = None
+    _need_tearDown = None
 
     def setup(self):
         self._testcase = self.parent.obj(self.name)
@@ -115,6 +117,8 @@ class TestCaseFunction(Function):
             self._request._fillfixtures()
 
     def teardown(self):
+        if self._need_tearDown:
+            self._testcase.tearDown()
         self._testcase = None
         self._obj = None
 
@@ -187,29 +191,45 @@ class TestCaseFunction(Function):
     def stopTest(self, testcase):
         pass
 
-    def _handle_skip(self):
-        # implements the skipping machinery (see #2137)
-        # analog to pythons Lib/unittest/case.py:run
-        testMethod = getattr(self._testcase, self._testcase._testMethodName)
-        if getattr(self._testcase.__class__, "__unittest_skip__", False) or getattr(
-            testMethod, "__unittest_skip__", False
-        ):
-            # If the class or method was skipped.
-            skip_why = getattr(
-                self._testcase.__class__, "__unittest_skip_why__", ""
-            ) or getattr(testMethod, "__unittest_skip_why__", "")
-            self._testcase._addSkip(self, self._testcase, skip_why)
-            return True
-        return False
-
     def runtest(self):
-        if self.config.pluginmanager.get_plugin("pdbinvoke") is None:
+        testMethod = getattr(self._testcase, self._testcase._testMethodName)
+
+        class _GetOutOf_testPartExecutor(KeyboardInterrupt):
+            """Helper exception to get out of unittests's testPartExecutor."""
+
+        unittest = sys.modules.get("unittest")
+
+        reraise = ()
+        if unittest:
+            reraise += (unittest.SkipTest,)
+
+        @functools.wraps(testMethod)
+        def wrapped_testMethod(*args, **kwargs):
+            try:
+                self.ihook.pytest_pyfunc_call(pyfuncitem=self)
+            except reraise:
+                raise
+            except Exception as exc:
+                expecting_failure_method = getattr(
+                    testMethod, "__unittest_expecting_failure__", False
+                )
+                expecting_failure_class = getattr(
+                    self, "__unittest_expecting_failure__", False
+                )
+                expecting_failure = expecting_failure_class or expecting_failure_method
+                self._need_tearDown = True
+
+                if expecting_failure:
+                    raise
+
+                raise _GetOutOf_testPartExecutor(exc)
+
+        self._testcase._wrapped_testMethod = wrapped_testMethod
+        self._testcase._testMethodName = "_wrapped_testMethod"
+        try:
             self._testcase(result=self)
-        else:
-            # disables tearDown and cleanups for post mortem debugging (see #1890)
-            if self._handle_skip():
-                return
-            self._testcase.debug()
+        except _GetOutOf_testPartExecutor as exc:
+            raise exc.args[0] from exc.args[0]
 
     def _prunetraceback(self, excinfo):
         Function._prunetraceback(self, excinfo)

testing/test_unittest.py
@@ -537,24 +537,28 @@ class TestTrialUnittest:
         )
         result.stdout.fnmatch_lines(
             [
-                "test_trial_error.py::TC::test_four FAILED",
+                "test_trial_error.py::TC::test_four SKIPPED",
                 "test_trial_error.py::TC::test_four ERROR",
                 "test_trial_error.py::TC::test_one FAILED",
                 "test_trial_error.py::TC::test_three FAILED",
-                "test_trial_error.py::TC::test_two FAILED",
+                "test_trial_error.py::TC::test_two SKIPPED",
+                "test_trial_error.py::TC::test_two ERROR",
                 "*ERRORS*",
                 "*_ ERROR at teardown of TC.test_four _*",
+                "NOTE: Incompatible Exception Representation, displaying natively:",
+                "*DelayedCalls*",
+                "*_ ERROR at teardown of TC.test_two _*",
+                "NOTE: Incompatible Exception Representation, displaying natively:",
                 "*DelayedCalls*",
                 "*= FAILURES =*",
-                "*_ TC.test_four _*",
-                "*NameError*crash*",
+                # "*_ TC.test_four _*",
+                # "*NameError*crash*",
                 "*_ TC.test_one _*",
                 "*NameError*crash*",
                 "*_ TC.test_three _*",
+                "NOTE: Incompatible Exception Representation, displaying natively:",
                 "*DelayedCalls*",
-                "*_ TC.test_two _*",
-                "*NameError*crash*",
-                "*= 4 failed, 1 error in *",
+                "*= 2 failed, 2 skipped, 2 errors in *",
             ]
         )
 
@@ -1096,3 +1100,32 @@ def test_exit_outcome(testdir):
     )
     result = testdir.runpytest()
     result.stdout.fnmatch_lines(["*Exit: pytest_exit called*", "*= no tests ran in *"])
+
+
+def test_trace(testdir, monkeypatch):
+    calls = []
+
+    def check_call(*args, **kwargs):
+        calls.append((args, kwargs))
+        assert args == ("runcall",)
+
+        class _pdb:
+            def runcall(*args, **kwargs):
+                calls.append((args, kwargs))
+
+        return _pdb
+
+    monkeypatch.setattr("_pytest.debugging.pytestPDB._init_pdb", check_call)
+
+    p1 = testdir.makepyfile(
+        """
+        import unittest
+
+        class MyTestCase(unittest.TestCase):
+            def test(self):
+                self.assertEqual('foo', 'foo')
+    """
+    )
+    result = testdir.runpytest("--trace", str(p1))
+    assert len(calls) == 2
+    assert result.ret == 0

commit e4d966971e4a5619bca4dc176ed8450e430cf919

unittest: fix the `_GetOutOf_testPartExecutor` exception kind. Fixes https://github.com/pytest-dev/pytest/issues/6947
`_GetOutOf_testPartExecutor` was introduced in
https://github.com/pytest-dev/pytest/commit/04f27d4eb469fb9c76fd2d100564a0f7d30028df
and allow to catch Exception from a unittest TestCase, but as it
inheritate from `KeyboardInterrupt` it is managed differently by
unittest (see.
https://github.com/python/cpython/blob/032de7324e30c6b44ef272cea3be205a3d768759/Lib/unittest/case.py#L61-L62)
as the `testPartExecutor` reraise it directly. This prevent unittest to
run the cleanup functions properly.

Since this change enable the teardown methods to be ran properly by
unittest, the explicit teardown is not required anymore.

AUTHORS
@@ -220,6 +220,7 @@ Pieter Mulder
 Piotr Banaszkiewicz
 Pulkit Goyal
 Punyashloka Biswal
+Quentin Bey
 Quentin Pradet
 Ralf Schmitt
 Ralph Giles

src/_pytest/unittest.py
@@ -113,15 +113,12 @@ class TestCaseFunction(Function):
     _testcase = None
 
     def setup(self):
-        self._needs_explicit_tearDown = False
         self._testcase = self.parent.obj(self.name)
         self._obj = getattr(self._testcase, self.name)
         if hasattr(self, "_request"):
             self._request._fillfixtures()
 
     def teardown(self):
-        if self._needs_explicit_tearDown:
-            self._testcase.tearDown()
         self._testcase = None
         self._obj = None
 
@@ -209,7 +206,7 @@ class TestCaseFunction(Function):
 
         testMethod = getattr(self._testcase, self._testcase._testMethodName)
 
-        class _GetOutOf_testPartExecutor(KeyboardInterrupt):
+        class _GetOutOf_testPartExecutor(Exception):
             """Helper exception to get out of unittests's testPartExecutor (see TestCase.run)."""
 
         @functools.wraps(testMethod)
@@ -218,13 +215,12 @@ class TestCaseFunction(Function):
             features can have a chance to kick in (notably --pdb)"""
             try:
                 self.ihook.pytest_pyfunc_call(pyfuncitem=self)
-            except unittest.SkipTest:
+            except (unittest.SkipTest, exit.Exception):
                 raise
             except Exception as exc:
                 expecting_failure = self._expecting_failure(testMethod)
                 if expecting_failure:
                     raise
-                self._needs_explicit_tearDown = True
                 raise _GetOutOf_testPartExecutor(exc)
 
         setattr(self._testcase, self._testcase._testMethodName, wrapped_testMethod)

testing/test_unittest.py
@@ -209,6 +209,91 @@ def test_teardown_issue1649(testdir):
         assert type(obj).__name__ != "TestCaseObjectsShouldBeCleanedUp"
 
 
+def test_cleanup_called_issue6947(testdir):
+    """
+    Are unittest.TestCase cleanup functions not invoked on test failure?
+    When a test fails and the cleanup is not called, all following tests may fail.
+    This is truly visible for test using a database when the transaction is not rolled back at the end.
+    """
+    testpath = testdir.makepyfile(
+        """
+        import unittest
+        import pytest
+
+        def increment_cleanup_count():
+            TestCaseObjectsShouldBeCleanedUp.cleanup_counter += 1
+
+        class TestCaseObjectsShouldBeCleanedUp(unittest.TestCase):
+            cleanup_counter = 0
+            def setUp(self):
+                self.addCleanup(increment_cleanup_count)
+            def test_1_goes_well(self):
+                # First test to be run
+                assert TestCaseObjectsShouldBeCleanedUp.cleanup_counter == 0
+            def test_2_that_fails(self):
+                # Second test to be run, should fail
+                assert False
+            def test_3_goes_well_after_failure(self):
+                # Third test to be run
+                assert TestCaseObjectsShouldBeCleanedUp.cleanup_counter == 2
+            def test_4_exit(self):
+                pytest.exit("pytest_exit called")
+            def test_5_not_run(self):
+                assert True
+    """
+    )
+    reprec = testdir.inline_run(testpath)
+    passed, skipped, failed = reprec.countoutcomes()
+    # test_1_goes_well passed
+    # test_2_that_fails failed
+    # test_3_goes_well_after_failure passed
+    # and finally test_4 quit
+    assert failed == 1, failed
+    assert passed == 2
+    assert passed + skipped + failed == 3
+
+
+def test_teardown_called_issue6947(testdir):
+    """
+    This test assert the TestCase.tearDown method is called once after every tests.
+
+    Looks very similar to test_cleanup_called_issue6947 but testing a different mechanism.
+    """
+    testpath = testdir.makepyfile(
+        """
+        import unittest
+        import pytest
+
+        class TestCaseObjectsShouldBeCleanedUp(unittest.TestCase):
+            cleanup_counter = 0
+            def tearDown(self):
+                TestCaseObjectsShouldBeCleanedUp.cleanup_counter += 1
+            def test_1_goes_well(self):
+                # First test to be run
+                assert TestCaseObjectsShouldBeCleanedUp.cleanup_counter == 0
+            def test_2_that_fails(self):
+                # Second test to be run, should fail
+                assert False
+            def test_3_goes_well_after_failure(self):
+                # Third test to be run
+                assert TestCaseObjectsShouldBeCleanedUp.cleanup_counter == 2
+            def test_4_exit(self):
+                pytest.exit("pytest_exit called")
+            def test_5_not_run(self):
+                assert True
+    """
+    )
+    reprec = testdir.inline_run(testpath)
+    passed, skipped, failed = reprec.countoutcomes()
+    # test_1_goes_well passed
+    # test_2_that_fails failed
+    # test_3_goes_well_after_failure passed
+    # and finally test_4 quit
+    assert failed == 1, failed
+    assert passed == 2
+    assert passed + skipped + failed == 3
+
+
 def test_unittest_skip_issue148(testdir):
     testpath = testdir.makepyfile(
         """