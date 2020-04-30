bafc9bd58b0cd244ba23da51cea01a792c1ea641
Ran Benita <ran@unusedvar.com>
2020-04-30 14:47:34 +0300

    testing: merge code/test_terminal_writer.py into io/test_terminalwriter.py

--- testing/io/test_terminalwriter.py
+++ testing/io/test_terminalwriter.py
@@ -1,5 +1,6 @@
 import io
 import os
+import re
 import shutil
 import sys
 from typing import Generator
@@ -207,3 +208,26 @@ class TestTerminalWriterLineWidth:
         assert len(text) == 10
         tw.write(text)
         assert tw.width_of_current_line == 9
+
+
+@pytest.mark.parametrize(
+    "has_markup, expected",
+    [
+        pytest.param(
+            True, "{kw}assert{hl-reset} {number}0{hl-reset}\n", id="with markup"
+        ),
+        pytest.param(False, "assert 0\n", id="no markup"),
+    ],
+)
+def test_code_highlight(has_markup, expected, color_mapping):
+    f = io.StringIO()
+    tw = terminalwriter.TerminalWriter(f)
+    tw.hasmarkup = has_markup
+    tw._write_source(["assert 0"])
+    assert f.getvalue().splitlines(keepends=True) == color_mapping.format([expected])
+
+    with pytest.raises(
+        ValueError,
+        match=re.escape("indents size (2) should have same size as lines (1)"),
+    ):
+        tw._write_source(["assert 0"], [" ", " "])
