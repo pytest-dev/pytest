184528d0c28f2cde73a0457414c55218fd6a2ddf
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-05-10 13:06:36 +0300

    Fix tests to expected repr output

--- testing/test_setuponly.py
+++ testing/test_setuponly.py
@@ -148,10 +148,10 @@ def test_show_fixtures_with_parameters(testdir, mode):
 
     result.stdout.fnmatch_lines(
         [
-            "SETUP    S arg_same?foo?",
-            "TEARDOWN S arg_same?foo?",
-            "SETUP    S arg_same?bar?",
-            "TEARDOWN S arg_same?bar?",
+            "SETUP    S arg_same?'foo'?",
+            "TEARDOWN S arg_same?'foo'?",
+            "SETUP    S arg_same?'bar'?",
+            "TEARDOWN S arg_same?'bar'?",
         ]
     )
 
@@ -181,7 +181,7 @@ def test_show_fixtures_with_parameter_ids(testdir, mode):
     assert result.ret == 0
 
     result.stdout.fnmatch_lines(
-        ["SETUP    S arg_same?spam?", "SETUP    S arg_same?ham?"]
+        ["SETUP    S arg_same?'spam'?", "SETUP    S arg_same?'ham'?"]
     )
 
 
@@ -200,7 +200,9 @@ def test_show_fixtures_with_parameter_ids_function(testdir, mode):
     result = testdir.runpytest(mode, p)
     assert result.ret == 0
 
-    result.stdout.fnmatch_lines(["*SETUP    F foobar?FOO?", "*SETUP    F foobar?BAR?"])
+    result.stdout.fnmatch_lines(
+        ["*SETUP    F foobar?'FOO'?", "*SETUP    F foobar?'BAR'?"]
+    )
 
 
 def test_dynamic_fixture_request(testdir):
