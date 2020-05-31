9d3c0a220cced5c84d9f24edf1238655e076ce8f
Bruno Oliveira <nicoddemus@gmail.com>
2020-05-31 12:28:57 -0300

    Use ImportError in Python 3.5

--- testing/test_collection.py
+++ testing/test_collection.py
@@ -1412,10 +1412,13 @@ class TestImportModeImportlib:
         """
         self.setup_conftest_and_foo(testdir)
         result = testdir.runpytest("-v", "--import-mode=importlib")
+        exc_name = (
+            "ModuleNotFoundError" if sys.version_info[:2] > (3, 5) else "ImportError"
+        )
         result.stdout.fnmatch_lines(
             [
-                "*ModuleNotFoundError: No module named 'foo'",
-                "tests?test_foo.py:2: ModuleNotFoundError",
+                "*{}: No module named 'foo'".format(exc_name),
+                "tests?test_foo.py:2: {}".format(exc_name),
                 "* 1 failed in *",
             ]
         )
