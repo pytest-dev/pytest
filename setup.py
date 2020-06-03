34307fc09255499173c013209b2b5d48ed40551c
Bruno Oliveira <nicoddemus@gmail.com>
2020-06-03 19:12:18 -0300

    Sort install dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
-# TODO: if py gets upgrade to >=1.6,
-#       remove _width_of_current_line in terminal.py
 INSTALL_REQUIRES = [
-    "py>=1.5.0",
-    "packaging",
+    # keep this list sorted
+    "atomicwrites>=1.0;sys_platform=='win32'",
     "attrs>=17.4.0",  # should match oldattrs tox env.
-    "more-itertools>=4.0.0",
-    'atomicwrites>=1.0;sys_platform=="win32"',
-    'pathlib2>=2.2.0;python_version<"3.6"',
-    'colorama;sys_platform=="win32"',
-    "pluggy>=0.12,<1.0",
-    'importlib-metadata>=0.12;python_version<"3.8"',
+    "colorama;sys_platform=='win32'",
+    "importlib-metadata>=0.12;python_version<'3.8'",
     "iniconfig",
+    "more-itertools>=4.0.0",
+    "packaging",
+    "pathlib2>=2.2.0;python_version<'3.6'",
+    "pluggy>=0.12,<1.0",
+    "py>=1.5.0",  # remove _width_of_current_line in terminal.py when upgrading to py>=1.6
 ]
 
 
