8550c29180afb6f1138c81645f7d92f644aaf74d
antonblr <anton.hrynevich@percolate.com>
2020-12-22 20:27:00 -0800

    coverage: Include code that runs in subprocesses

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -10,9 +10,7 @@ else
   PATH="$PWD/.tox/${TOXENV##*,}/bin:$PATH"
 fi
 
-python -m coverage combine
 python -m coverage xml
-python -m coverage report -m
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
 curl -S -L --connect-timeout 5 --retry 6 -s https://codecov.io/bash -o codecov-upload.sh
 bash codecov-upload.sh -Z -X fix -f coverage.xml "$@"
