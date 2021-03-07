412fc001a0770d44b281d9b39f736df3cdc80c37
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2021-03-07 14:57:19 +0100

    fix bug in test for issue 519
    assert the actual outcome and fix the filename

--- testing/examples/test_issue519.py
+++ testing/examples/test_issue519.py
@@ -1,6 +1,7 @@
 from _pytest.pytester import Pytester
 
 
-def test_510(pytester: Pytester) -> None:
+def test_519(pytester: Pytester) -> None:
     pytester.copy_example("issue_519.py")
-    pytester.runpytest("issue_519.py")
+    res = pytester.runpytest("issue_519.py")
+    res.assert_outcomes(passed=8)
