0808d93d656a37cd7c2be08c2e201b78b603dad1
Ran Benita <ran@unusedvar.com>
2021-10-04 10:43:38 +0300

    Update src/_pytest/config/__init__.py

--- src/_pytest/config/__init__.py
+++ src/_pytest/config/__init__.py
@@ -254,9 +254,9 @@ default_plugins = essential_plugins + (
     "warnings",
     "logging",
     "reports",
+    "pythonpath",
     *(["unraisableexception", "threadexception"] if sys.version_info >= (3, 8) else []),
     "faulthandler",
-    "pythonpath",
 )
 
 builtin_plugins = set(default_plugins)
