747b8372eabc8909aae411937c6b80536bb214c4
Bruno Oliveira <nicoddemus@gmail.com>
2022-02-13 09:39:57 -0300

    Try to import module before creating dummy modules with 'importmode=importlib'
    The dummy modules we introduce in `insert_missing_modules` (due to #7856 and #7859)
    would cause problems if the dummy modules actually end up replacing modules
    which could be imported normally because they are available in `PYTHONPATH`.
    
    Now we attempt to first import the module via normal mechanisms, and only
    introduce the dummy modules if the intermediary modules don't actually exist.
    
    Close #9645

--- src/_pytest/pathlib.py
+++ src/_pytest/pathlib.py
@@ -603,11 +603,20 @@ def insert_missing_modules(modules: Dict[str, ModuleType], module_name: str) -> 
     module_parts = module_name.split(".")
     while module_name:
         if module_name not in modules:
-            module = ModuleType(
-                module_name,
-                doc="Empty module created by pytest's importmode=importlib.",
-            )
-            modules[module_name] = module
+            try:
+                # If sys.meta_path is empty, calling import_module will issue
+                # a warning and raise ModuleNotFoundError. To avoid the
+                # warning, we check sys.meta_path explicitly and raise the error
+                # ourselves to fall back to creating a dummy module.
+                if not sys.meta_path:
+                    raise ModuleNotFoundError
+                importlib.import_module(module_name)
+            except ModuleNotFoundError:
+                module = ModuleType(
+                    module_name,
+                    doc="Empty module created by pytest's importmode=importlib.",
+                )
+                modules[module_name] = module
         module_parts.pop(-1)
         module_name = ".".join(module_parts)
 
