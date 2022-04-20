715d0ca9d0aa6a6136487336193c98e2604a0353
Paul Müller <paulmueller@users.noreply.github.com>
2022-04-20 20:21:44 +0200

    cleanup

--- src/_pytest/tmpdir.py
+++ src/_pytest/tmpdir.py
@@ -161,7 +161,6 @@ def get_user() -> Optional[str]:
     try:
         # In some exotic environments, getpass may not be importable.
         import getpass
-
         return getpass.getuser()
     except (ImportError, KeyError):
         return None
