42db2b03f76d04ae24988604a3db2966dcdc0a5c
Itxaso Aizpurua <itsaso23a@gmail.com>
2022-10-08 09:15:58 +0200

    Not skip maxsize control of SafeRepr

--- src/_pytest/_io/saferepr.py
+++ src/_pytest/_io/saferepr.py
@@ -62,6 +62,7 @@ class SafeRepr(reprlib.Repr):
                 s = ascii(x)
             else:
                 s = super().repr(x)
+
         except (KeyboardInterrupt, SystemExit):
             raise
         except BaseException as exc:
