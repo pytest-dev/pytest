59da17ddd26fabad3c611a96ec8258b2014d6d8b
Thomas Grainger <tagrain@gmail.com>
2022-10-09 17:04:58 +0100

    remove sealed from compat

--- src/_pytest/compat.py
+++ src/_pytest/compat.py
@@ -342,12 +342,6 @@ else:
         return f
 
 
-# TODO: sealed classes are not supported yet
-# https://github.com/mypyc/mypyc/issues/846
-def sealed(f: _T) -> _T:
-    return f
-
-
 if sys.version_info >= (3, 8):
     from functools import cached_property as cached_property
 else:
