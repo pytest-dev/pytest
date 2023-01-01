1dd96577b215a881e5cf55740d5ca218b22c6b38
Jayendra <jndrpatil@gmail.com>
2023-01-01 16:16:15 +0530

    added a check if denominator is zero

--- src/_pytest/python_api.py
+++ src/_pytest/python_api.py
@@ -266,14 +266,17 @@ class ApproxMapping(ApproxBase):
             approx_side_as_map.items(), other_side.values()
         ):
             if approx_value != other_value:
-                max_abs_diff = max(
-                    max_abs_diff, abs(approx_value.expected - other_value)
-                )
-                max_rel_diff = max(
-                    max_rel_diff,
-                    abs((approx_value.expected - other_value) / approx_value.expected),
-                )
-                different_ids.append(approx_key)
+              max_abs_diff = max(
+                  max_abs_diff, abs(approx_value.expected - other_value)
+              )
+              if approx_value.expected == 0.0:
+                  max_rel_diff = math.inf
+              else:
+                  max_rel_diff = max(
+                      max_rel_diff,
+                      abs((approx_value.expected - other_value) / approx_value.expected),
+                  )
+              different_ids.append(approx_key)
 
         message_data = [
             (str(key), str(other_side[key]), str(approx_side_as_map[key]))
