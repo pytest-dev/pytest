1a96f16401c7bf10d4873f54b02f7bbbf94f492d
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2023-01-13 09:29:38 -0500

    Derive pytest.raises from AbstractContextManager
    Makes `AbstractContextManager` the shared base class between "raises" and other context managers.
    
    The motivation is for type checkers to narrow `pytest.raises(...) if x else nullcontext()` to a `ContextManager` rather than `object`.

--- testing/typing_checks.py
+++ testing/typing_checks.py
@@ -3,6 +3,11 @@
 This file is not executed, it is only checked by mypy to ensure that
 none of the code triggers any mypy errors.
 """
+import contextlib
+from typing import Optional
+
+from typing_extensions import assert_type
+
 import pytest
 
 
@@ -22,3 +27,9 @@ def check_fixture_ids_callable() -> None:
 @pytest.mark.parametrize("func", [str, int], ids=lambda x: str(x.__name__))
 def check_parametrize_ids_callable(func) -> None:
     pass
+
+
+def check_raises_is_a_context_manager(val: bool) -> None:
+    with pytest.raises(RuntimeError) if val else contextlib.nullcontext() as excinfo:
+        pass
+    assert_type(excinfo, Optional[pytest.ExceptionInfo[RuntimeError]])
