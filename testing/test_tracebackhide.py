a583c68e150626340b41d890ffd9f3049e5c83ff
Felix Hofstätter <felixahofstaetter@gmail.com>
2023-02-28 02:07:17 +0100

    Correctly handle tracebackhide for chained exceptions

---
+++ testing/test_tracebackhide.py
@@ -0,0 +1,24 @@
+def test_tbh_chained(testdir):
+    p = testdir.makepyfile(
+        """
+        import pytest
+
+        def f1():
+            __tracebackhide__ = True
+            try:
+                return f1.meh
+            except AttributeError:
+                pytest.fail("fail")
+
+        @pytest.fixture
+        def fix():
+            f1()
+
+
+        def test(fix):
+            pass
+        """
+    )
+    result = testdir.runpytest(str(p))
+    assert "'function' object has no attribute 'meh'" not in result.stdout.str()
+    assert result.ret == 1
