ba3f034ecc0b855c9fff1bf31e6bc102ade527d6
Bruno Oliveira <nicoddemus@gmail.com>
2023-07-20 08:23:14 -0300

    Update src/_pytest/python_api.py

--- src/_pytest/python_api.py
+++ src/_pytest/python_api.py
@@ -812,8 +812,8 @@ def raises(  # noqa: F811
     :kwparam str | typing.Pattern[str] | None match:
         If specified, a string containing a regular expression,
         or a regular expression object, that is tested against the string
-        representation or `PEP-678 <https://peps.python.org/pep-0678/>` `__notes__`
-        of the exception using :func:`re.search`.
+        representation of the exception and its `PEP-678 <https://peps.python.org/pep-0678/>` `__notes__`
+        using :func:`re.search`.
 
         To match a literal string that may contain :ref:`special characters
         <re-syntax>`, the pattern can first be escaped with :func:`re.escape`.
