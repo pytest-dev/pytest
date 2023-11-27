2d1710e0e93983134e83e26eacff6a147164baf1
Benjamin Schubert <bschubert15@bloomberg.net>
2023-11-27 14:47:18 +0000

    Improve the full diff by having more consistent indentation in the PrettyPrinter (#11571)
    The normal default pretty printer is not great when objects are nested
    and it can get hard to read the diff.
    
    Instead, provide a pretty printer that behaves more like when json get
    indented, which allows for smaller, more meaningful differences, at
    the expense of a slightly longer diff.
    
    This does not touch the other places where the pretty printer is used,
    and only updated the full diff one.

--- src/_pytest/assertion/util.py
+++ src/_pytest/assertion/util.py
@@ -318,18 +318,6 @@ def _diff_text(left: str, right: str, verbose: int = 0) -> List[str]:
     return explanation
 
 
-def _surrounding_parens_on_own_lines(lines: List[str]) -> None:
-    """Move opening/closing parenthesis/bracket to own lines."""
-    opening = lines[0][:1]
-    if opening in ["(", "[", "{"]:
-        lines[0] = " " + lines[0][1:]
-        lines[:] = [opening] + lines
-    closing = lines[-1][-1:]
-    if closing in [")", "]", "}"]:
-        lines[-1] = lines[-1][:-1] + ","
-        lines[:] = lines + [closing]
-
-
 def _compare_eq_iterable(
     left: Iterable[Any],
     right: Iterable[Any],
@@ -341,20 +329,8 @@ def _compare_eq_iterable(
     # dynamic import to speedup pytest
     import difflib
 
-    left_formatting = pprint.pformat(left).splitlines()
-    right_formatting = pprint.pformat(right).splitlines()
-
-    # Re-format for different output lengths.
-    lines_left = len(left_formatting)
-    lines_right = len(right_formatting)
-    if lines_left != lines_right:
-        printer = PrettyPrinter()
-        left_formatting = printer.pformat(left).splitlines()
-        right_formatting = printer.pformat(right).splitlines()
-
-    if lines_left > 1 or lines_right > 1:
-        _surrounding_parens_on_own_lines(left_formatting)
-        _surrounding_parens_on_own_lines(right_formatting)
+    left_formatting = PrettyPrinter().pformat(left).splitlines()
+    right_formatting = PrettyPrinter().pformat(right).splitlines()
 
     explanation = ["Full diff:"]
     # "right" is the expected base against which we compare "left",
