992d0f082f43c06f6a50bf9c863eba2d221d2272
Ran Benita <ran@unusedvar.com>
2024-01-06 13:12:02 +0200

    fixtures: match fixtures based on actual node hierarchy, not textual nodeids
    Refs #11662.
    
    --- Problem
    
    Each fixture definition has a "visibility", the `FixtureDef.baseid`
    attribute. This is nodeid-like string. When a certain `node` requests a
    certain fixture name, we match node's nodeid against the fixture
    definitions with this name.
    
    The matching currently happens on the *textual* representation of the
    nodeid - we split `node.nodeid` to its "parent nodeids" and then check
    if the fixture's `baseid` is in there.
    
    While this has worked so far, we really should try to avoid textual
    manipulation of nodeids as much as possible. It has also caused problem
    in an odd case of a `Package` in the root directory: the `Package` gets
    nodeid `.`, while a `Module` in it gets nodeid `test_module.py`. And
    textually, `.` is not a parent of `test_module.py`.
    
    --- Solution
    
    Avoid this entirely by just checking the node hierarchy itself. This is
    made possible by the fact that we now have proper `Directory` nodes
    (`Dir` or `Package`) for the entire hierarchy.
    
    Also do the same for `_getautousenames` which is a similar deal.
    
    The `iterparentnodeids` function is no longer used and is removed.

--- testing/test_nodes.py
+++ testing/test_nodes.py
@@ -2,7 +2,6 @@ import re
 import warnings
 from pathlib import Path
 from typing import cast
-from typing import List
 from typing import Type
 
 import pytest
@@ -10,29 +9,6 @@ from _pytest import nodes
 from _pytest.outcomes import OutcomeException
 from _pytest.pytester import Pytester
 from _pytest.warning_types import PytestWarning
-
-
-@pytest.mark.parametrize(
-    ("nodeid", "expected"),
-    (
-        ("", [""]),
-        ("a", ["", "a"]),
-        ("aa/b", ["", "aa", "aa/b"]),
-        ("a/b/c", ["", "a", "a/b", "a/b/c"]),
-        ("a/bbb/c::D", ["", "a", "a/bbb", "a/bbb/c", "a/bbb/c::D"]),
-        ("a/b/c::D::eee", ["", "a", "a/b", "a/b/c", "a/b/c::D", "a/b/c::D::eee"]),
-        ("::xx", ["", "::xx"]),
-        # / only considered until first ::
-        ("a/b/c::D/d::e", ["", "a", "a/b", "a/b/c", "a/b/c::D/d", "a/b/c::D/d::e"]),
-        # : alone is not a separator.
-        ("a/b::D:e:f::g", ["", "a", "a/b", "a/b::D:e:f", "a/b::D:e:f::g"]),
-        # / not considered if a part of a test name
-        ("a/b::c/d::e[/test]", ["", "a", "a/b", "a/b::c/d", "a/b::c/d::e[/test]"]),
-    ),
-)
-def test_iterparentnodeids(nodeid: str, expected: List[str]) -> None:
-    result = list(nodes.iterparentnodeids(nodeid))
-    assert result == expected
 
 
 def test_node_from_parent_disallowed_arguments() -> None:
