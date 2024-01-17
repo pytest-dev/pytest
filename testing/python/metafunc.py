af2cf2b46ba38deb574fed4d919a51fb25875852
Ran Benita <ran@unusedvar.com>
2024-01-17 22:35:57 +0200

    testing: temporarily disable test due to hypothesis issue
    Ref: https://github.com/pytest-dev/pytest/pull/11825#issuecomment-1894094641

--- testing/python/metafunc.py
+++ testing/python/metafunc.py
@@ -13,9 +13,6 @@ from typing import Sequence
 from typing import Tuple
 from typing import Union
 
-import hypothesis
-from hypothesis import strategies
-
 import pytest
 from _pytest import fixtures
 from _pytest import python
@@ -26,6 +23,8 @@ from _pytest.pytester import Pytester
 from _pytest.python import Function
 from _pytest.python import IdMaker
 from _pytest.scope import Scope
+# import hypothesis
+# from hypothesis import strategies
 
 
 class TestMetafunc:
@@ -292,14 +291,15 @@ class TestMetafunc:
         assert metafunc._calls[2].id == "x1-a"
         assert metafunc._calls[3].id == "x1-b"
 
-    @hypothesis.given(strategies.text() | strategies.binary())
-    @hypothesis.settings(
-        deadline=400.0
-    )  # very close to std deadline and CI boxes are not reliable in CPU power
-    def test_idval_hypothesis(self, value) -> None:
-        escaped = IdMaker([], [], None, None, None, None, None)._idval(value, "a", 6)
-        assert isinstance(escaped, str)
-        escaped.encode("ascii")
+    # TODO: Uncomment - https://github.com/HypothesisWorks/hypothesis/pull/3849
+    # @hypothesis.given(strategies.text() | strategies.binary())
+    # @hypothesis.settings(
+    #     deadline=400.0
+    # )  # very close to std deadline and CI boxes are not reliable in CPU power
+    # def test_idval_hypothesis(self, value) -> None:
+    #     escaped = IdMaker([], [], None, None, None, None, None)._idval(value, "a", 6)
+    #     assert isinstance(escaped, str)
+    #     escaped.encode("ascii")
 
     def test_unicode_idval(self) -> None:
         """Test that Unicode strings outside the ASCII character set get
