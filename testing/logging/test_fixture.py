77647e48588a509534950ca4cf0a00164bb9716b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-11 09:13:31 -0400

    Refactor private_assert_caplog_records_is_setup_call
    Ensure all uses of caplog._item have a comment highlighting it is
    a private API and not for use in real tests!

--- testing/logging/test_fixture.py
+++ testing/logging/test_fixture.py
@@ -302,6 +302,12 @@ def logging_during_setup_and_teardown(
     assert [x.message for x in caplog.get_records("teardown")] == ["a_teardown_log"]
 
 
+def private_assert_caplog_records_is_setup_call(caplog: pytest.LogCaptureFixture) -> None:
+    # This reaches into private API, don't use this type of thing in real tests!
+    caplog_records = caplog._item.stash[caplog_records_key]
+    assert set(caplog_records) == {"setup", "call"}
+
+
 def test_caplog_captures_for_all_stages(
     caplog: pytest.LogCaptureFixture, logging_during_setup_and_teardown: None
 ) -> None:
@@ -312,10 +318,7 @@ def test_caplog_captures_for_all_stages(
 
     assert [x.message for x in caplog.get_records("setup")] == ["a_setup_log"]
 
-    # This reaches into private API, don't use this type of thing in real tests!
-    caplog_records = caplog._item.stash[caplog_records_key]
-    assert set(caplog_records) == {"setup", "call"}
-
+    private_assert_caplog_records_is_setup_call(caplog)
 
 def test_clear_for_call_stage(
     caplog: pytest.LogCaptureFixture, logging_during_setup_and_teardown: None
@@ -323,21 +326,18 @@ def test_clear_for_call_stage(
     logger.info("a_call_log")
     assert [x.message for x in caplog.get_records("call")] == ["a_call_log"]
     assert [x.message for x in caplog.get_records("setup")] == ["a_setup_log"]
-    caplog_records = caplog._item.stash[caplog_records_key]
-    assert set(caplog_records) == {"setup", "call"}
+    private_assert_caplog_records_is_setup_call(caplog)
 
     caplog.clear()
 
     assert caplog.get_records("call") == []
     assert [x.message for x in caplog.get_records("setup")] == ["a_setup_log"]
-    caplog_records = caplog._item.stash[caplog_records_key]
-    assert set(caplog_records) == {"setup", "call"}
+    private_assert_caplog_records_is_setup_call(caplog)
 
     logging.info("a_call_log_after_clear")
     assert [x.message for x in caplog.get_records("call")] == ["a_call_log_after_clear"]
     assert [x.message for x in caplog.get_records("setup")] == ["a_setup_log"]
-    caplog_records = caplog._item.stash[caplog_records_key]
-    assert set(caplog_records) == {"setup", "call"}
+    private_assert_caplog_records_is_setup_call(caplog)
 
 
 def test_ini_controls_global_log_level(pytester: Pytester) -> None:
