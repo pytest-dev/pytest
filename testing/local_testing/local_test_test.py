ababe237fd4b09f502e86bec525545be68856c75
Chaeil Yun <chaeily@andrew.cmu.edu>
2024-04-25 20:28:49 -0400

    some changing up

--- testing/local_testing/local_test_test.py
+++ testing/local_testing/local_test_test.py
@@ -1,13 +1,22 @@
 # test_math_operations.py
 
+import pytest
 import add_function
 
-import pytest
 
-
-result_index = [(1, 2, 3), (-1, 1, 0), (0, 0, 0), (5, -3, 2), (1, 1, 2)]
-
+result_index = [
+    (1, 2, 3),
+    (-1, 1, 0),
+    (0, 0, 0),
+    (5, -3, 2),
+    (1, 1, 2)
+]
 
 @pytest.mark.parametrize("a, b, expected_result", result_index)
+
+
 def test_add(a, b, expected_result):
     assert add_function.add(a, b) == expected_result
+
+
+
