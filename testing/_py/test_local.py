5f1f3aa6f83758d571a1c95d8ed4377d78597a2f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2024-05-27 23:19:43 +0200

    Put back the original test intent into place using 3.10 instead of 3.8
    Taking into account https://github.com/pytest-dev/pytest/pull/12172\#discussion_r1575926834

--- testing/_py/test_local.py
+++ testing/_py/test_local.py
@@ -205,7 +205,10 @@ class CommonFSTests:
         assert "sampledir" in lst
         assert path1.sep.join(["sampledir", "otherfile"]) not in lst
 
-    @pytest.mark.parametrize("fil", ["*dir", "*dir", b"*dir"])
+    @pytest.mark.parametrize(
+        "fil",
+        ["*dir", "*dir", pytest.mark.skipif("sys.version_info < (3,10)")(b"*dir")],
+    )
     def test_visit_filterfunc_is_string(self, path1, fil):
         lst = []
         for i in path1.visit(fil):
