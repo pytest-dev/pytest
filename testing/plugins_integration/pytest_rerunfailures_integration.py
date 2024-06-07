31791676501f8a8f9eb0d316a6d841a4c0d6738d
Ran Benita <ran@unusedvar.com>
2024-06-08 02:11:20 +0300

    [8.2.x] unittest: fix assertion errors on unittest reruns

---
+++ testing/plugins_integration/pytest_rerunfailures_integration.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    first_time = True
+
+    def test_fail_the_first_time(self) -> None:
+        """Regression test for issue #12424."""
+        if self.first_time:
+            type(self).first_time = False
+            self.fail()
