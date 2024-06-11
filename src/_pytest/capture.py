pytest-dev/pytest

commit 4588653b2497ed25976b7aaff225b889fb476756

Migrate from autoflake, black, isort, pyupgrade, flake8 and pydocstyle, to ruff
ruff is faster and handle everything we had prior.

isort configuration done based on the indication from
https://github.com/astral-sh/ruff/issues/4670, previousely based on
reorder-python-import (#11896)

flake8-docstrings was a wrapper around pydocstyle (now archived) that
explicitly asks to use ruff in https://github.com/PyCQA/pydocstyle/pull/658.

flake8-typing-import is useful mainly for project that support python 3.7
and the one useful check will be implemented in https://github.com/astral-sh/ruff/issues/2302

We need to keep blacken-doc because ruff does not handle detection
of python code inside .md and .rst. The direct link to the repo is
now used to avoid a redirection.

Manual fixes:
- Lines that became too long
- % formatting that was not done automatically
- type: ignore that were moved around
- noqa of hard to fix issues (UP031 generally)
- fmt: off and fmt: on that is not really identical
  between black and ruff
- autofix re-order in pre-commit from faster to slower

Co-authored-by: Ran Benita <ran@unusedvar.com>

src/_pytest/capture.py
@@ -4,9 +4,9 @@ import abc
 import collections
 import contextlib
 import io
+from io import UnsupportedOperation
 import os
 import sys
-from io import UnsupportedOperation
 from tempfile import TemporaryFile
 from types import TracebackType
 from typing import Any
@@ -38,6 +38,7 @@ from _pytest.nodes import Collector
 from _pytest.nodes import File
 from _pytest.nodes import Item
 from _pytest.reports import CollectReport
+
 
 _CaptureMethod = Literal["fd", "sys", "no", "tee-sys"]
 
@@ -790,9 +791,7 @@ class CaptureManager:
             current_fixture = self._capture_fixture.request.fixturename
             requested_fixture = capture_fixture.request.fixturename
             capture_fixture.request.raiseerror(
-                "cannot use {} and {} at the same time".format(
-                    requested_fixture, current_fixture
-                )
+                f"cannot use {requested_fixture} and {current_fixture} at the same time"
             )
         self._capture_fixture = capture_fixture
 
@@ -988,7 +987,6 @@ def capsys(request: SubRequest) -> Generator[CaptureFixture[str], None, None]:
     Returns an instance of :class:`CaptureFixture[str] <pytest.CaptureFixture>`.
 
     Example:
-
     .. code-block:: python
 
         def test_output(capsys):
@@ -1016,7 +1014,6 @@ def capsysbinary(request: SubRequest) -> Generator[CaptureFixture[bytes], None, 
     Returns an instance of :class:`CaptureFixture[bytes] <pytest.CaptureFixture>`.
 
     Example:
-
     .. code-block:: python
 
         def test_output(capsysbinary):
@@ -1044,7 +1041,6 @@ def capfd(request: SubRequest) -> Generator[CaptureFixture[str], None, None]:
     Returns an instance of :class:`CaptureFixture[str] <pytest.CaptureFixture>`.
 
     Example:
-
     .. code-block:: python
 
         def test_system_echo(capfd):
@@ -1072,7 +1068,6 @@ def capfdbinary(request: SubRequest) -> Generator[CaptureFixture[bytes], None, N
     Returns an instance of :class:`CaptureFixture[bytes] <pytest.CaptureFixture>`.
 
     Example:
-
     .. code-block:: python
 
         def test_system_echo(capfdbinary):

src/_pytest/config/__init__.py
@@ -5,18 +5,17 @@ import collections.abc
 import copy
 import dataclasses
 import enum
+from functools import lru_cache
 import glob
 import importlib.metadata
 import inspect
 import os
+from pathlib import Path
 import re
 import shlex
 import sys
-import types
-import warnings
-from functools import lru_cache
-from pathlib import Path
 from textwrap import dedent
+import types
 from types import FunctionType
 from typing import Any
 from typing import Callable
@@ -37,6 +36,7 @@ from typing import Tuple
 from typing import Type
 from typing import TYPE_CHECKING
 from typing import Union
+import warnings
 
 from pluggy import HookimplMarker
 from pluggy import HookimplOpts
@@ -44,15 +44,15 @@ from pluggy import HookspecMarker
 from pluggy import HookspecOpts
 from pluggy import PluginManager
 
-import _pytest._code
-import _pytest.deprecated
-import _pytest.hookspec
 from .exceptions import PrintHelp as PrintHelp
 from .exceptions import UsageError as UsageError
 from .findpaths import determine_setup
+import _pytest._code
 from _pytest._code import ExceptionInfo
 from _pytest._code import filter_traceback
 from _pytest._io import TerminalWriter
+import _pytest.deprecated
+import _pytest.hookspec
 from _pytest.outcomes import fail
 from _pytest.outcomes import Skipped
 from _pytest.pathlib import absolutepath
@@ -64,6 +64,7 @@ from _pytest.pathlib import safe_exists
 from _pytest.stash import Stash
 from _pytest.warning_types import PytestConfigWarning
 from _pytest.warning_types import warn_explicit_for
+
 
 if TYPE_CHECKING:
     from .argparsing import Argument
@@ -813,7 +814,7 @@ class PytestPluginManager(PluginManager):
 
 
 def _get_plugin_specs_as_list(
-    specs: Union[None, types.ModuleType, str, Sequence[str]]
+    specs: Union[None, types.ModuleType, str, Sequence[str]],
 ) -> List[str]:
     """Parse a plugins specification into a list of plugin names."""
     # None means empty.
@@ -1374,12 +1375,7 @@ class Config:
 
             if Version(minver) > Version(pytest.__version__):
                 raise pytest.UsageError(
-                    "%s: 'minversion' requires pytest-%s, actual pytest-%s'"
-                    % (
-                        self.inipath,
-                        minver,
-                        pytest.__version__,
-                    )
+                    f"{self.inipath}: 'minversion' requires pytest-{minver}, actual pytest-{pytest.__version__}'"
                 )
 
     def _validate_config_options(self) -> None:
@@ -1614,9 +1610,7 @@ class Config:
                 key, user_ini_value = ini_config.split("=", 1)
             except ValueError as e:
                 raise UsageError(
-                    "-o/--override-ini expects option=value style (got: {!r}).".format(
-                        ini_config
-                    )
+                    f"-o/--override-ini expects option=value style (got: {ini_config!r})."
                 ) from e
             else:
                 if key == name:
@@ -1674,7 +1668,6 @@ class Config:
         can be used to explicitly use the global verbosity level.
 
         Example:
-
         .. code-block:: ini
 
             # content of pytest.ini

src/_pytest/monkeypatch.py
@@ -1,10 +1,9 @@
 # mypy: allow-untyped-defs
 """Monkeypatching and mocking functionality."""
+from contextlib import contextmanager
 import os
 import re
 import sys
-import warnings
-from contextlib import contextmanager
 from typing import Any
 from typing import final
 from typing import Generator
@@ -16,9 +15,11 @@ from typing import overload
 from typing import Tuple
 from typing import TypeVar
 from typing import Union
+import warnings
 
 from _pytest.fixtures import fixture
 from _pytest.warning_types import PytestWarning
+
 
 RE_IMPORT_ERROR_NAME = re.compile(r"^No module named (.*)$")
 
@@ -90,9 +91,7 @@ def annotated_getattr(obj: object, name: str, ann: str) -> object:
         obj = getattr(obj, name)
     except AttributeError as e:
         raise AttributeError(
-            "{!r} object at {} has no attribute {!r}".format(
-                type(obj).__name__, ann, name
-            )
+            f"{type(obj).__name__!r} object at {ann} has no attribute {name!r}"
         ) from e
     return obj
 
@@ -142,7 +141,6 @@ class MonkeyPatch:
         which undoes any patching done inside the ``with`` block upon exit.
 
         Example:
-
         .. code-block:: python
 
             import functools
@@ -169,7 +167,8 @@ class MonkeyPatch:
         name: object,
         value: Notset = ...,
         raising: bool = ...,
-    ) -> None: ...
+    ) -> None:
+        ...
 
     @overload
     def setattr(
@@ -178,7 +177,8 @@ class MonkeyPatch:
         name: str,
         value: object,
         raising: bool = ...,
-    ) -> None: ...
+    ) -> None:
+        ...
 
     def setattr(
         self,
@@ -320,10 +320,8 @@ class MonkeyPatch:
         if not isinstance(value, str):
             warnings.warn(  # type: ignore[unreachable]
                 PytestWarning(
-                    "Value of environment variable {name} type should be str, but got "
-                    "{value!r} (type: {type}); converted to str implicitly".format(
-                        name=name, value=value, type=type(value).__name__
-                    )
+                    f"Value of environment variable {name} type should be str, but got "
+                    f"{value!r} (type: {type(value).__name__}); converted to str implicitly"
                 ),
                 stacklevel=2,
             )
@@ -343,7 +341,6 @@ class MonkeyPatch:
 
     def syspath_prepend(self, path) -> None:
         """Prepend ``path`` to ``sys.path`` list of import locations."""
-
         if self._savesyspath is None:
             self._savesyspath = sys.path[:]
         sys.path.insert(0, str(path))

src/_pytest/pytester.py
@@ -5,19 +5,19 @@ PYTEST_DONT_REWRITE
 """
 import collections.abc
 import contextlib
+from fnmatch import fnmatch
 import gc
 import importlib
+from io import StringIO
 import locale
 import os
+from pathlib import Path
 import platform
 import re
 import shutil
 import subprocess
 import sys
 import traceback
-from fnmatch import fnmatch
-from io import StringIO
-from pathlib import Path
 from typing import Any
 from typing import Callable
 from typing import Dict
@@ -69,6 +69,7 @@ from _pytest.reports import CollectReport
 from _pytest.reports import TestReport
 from _pytest.tmpdir import TempPathFactory
 from _pytest.warning_types import PytestWarning
+
 
 if TYPE_CHECKING:
     import pexpect
@@ -187,7 +188,7 @@ class LsofFdLeakChecker:
                     "*** After:",
                     *(str(f) for f in lines2),
                     "***** %s FD leakage detected" % len(leaked_files),
-                    "*** function %s:%s: %s " % item.location,
+                    "*** function {}:{}: {} ".format(*item.location),
                     "See issue #2366",
                 ]
                 item.warn(PytestWarning("\n".join(error)))
@@ -244,7 +245,8 @@ class RecordedHookCall:
 
     if TYPE_CHECKING:
         # The class has undetermined attributes, this tells mypy about it.
-        def __getattr__(self, key: str): ...
+        def __getattr__(self, key: str):
+            ...
 
 
 @final
@@ -325,13 +327,15 @@ class HookRecorder:
     def getreports(
         self,
         names: "Literal['pytest_collectreport']",
-    ) -> Sequence[CollectReport]: ...
+    ) -> Sequence[CollectReport]:
+        ...
 
     @overload
     def getreports(
         self,
         names: "Literal['pytest_runtest_logreport']",
-    ) -> Sequence[TestReport]: ...
+    ) -> Sequence[TestReport]:
+        ...
 
     @overload
     def getreports(
@@ -340,7 +344,8 @@ class HookRecorder:
             "pytest_collectreport",
             "pytest_runtest_logreport",
         ),
-    ) -> Sequence[Union[CollectReport, TestReport]]: ...
+    ) -> Sequence[Union[CollectReport, TestReport]]:
+        ...
 
     def getreports(
         self,
@@ -372,14 +377,12 @@ class HookRecorder:
                 values.append(rep)
         if not values:
             raise ValueError(
-                "could not find test report matching %r: "
-                "no test reports at all!" % (inamepart,)
+                f"could not find test report matching {inamepart!r}: "
+                "no test reports at all!"
             )
         if len(values) > 1:
             raise ValueError(
-                "found 2 or more testreports matching {!r}: {}".format(
-                    inamepart, values
-                )
+                f"found 2 or more testreports matching {inamepart!r}: {values}"
             )
         return values[0]
 
@@ -387,13 +390,15 @@ class HookRecorder:
     def getfailures(
         self,
         names: "Literal['pytest_collectreport']",
-    ) -> Sequence[CollectReport]: ...
+    ) -> Sequence[CollectReport]:
+        ...
 
     @overload
     def getfailures(
         self,
         names: "Literal['pytest_runtest_logreport']",
-    ) -> Sequence[TestReport]: ...
+    ) -> Sequence[TestReport]:
+        ...
 
     @overload
     def getfailures(
@@ -402,7 +407,8 @@ class HookRecorder:
             "pytest_collectreport",
             "pytest_runtest_logreport",
         ),
-    ) -> Sequence[Union[CollectReport, TestReport]]: ...
+    ) -> Sequence[Union[CollectReport, TestReport]]:
+        ...
 
     def getfailures(
         self,
@@ -801,7 +807,6 @@ class Pytester:
             The first created file.
 
         Examples:
-
         .. code-block:: python
 
             pytester.makefile(".txt", "line1", "line2")
@@ -855,7 +860,6 @@ class Pytester:
         existing files.
 
         Examples:
-
         .. code-block:: python
 
             def test_something(pytester):
@@ -875,7 +879,6 @@ class Pytester:
         existing files.
 
         Examples:
-
         .. code-block:: python
 
             def test_something(pytester):
@@ -1265,9 +1268,7 @@ class Pytester:
         for item in items:
             if item.name == funcname:
                 return item
-        assert 0, "{!r} item not found in module:\n{}\nitems: {}".format(
-            funcname, source, items
-        )
+        assert 0, f"{funcname!r} item not found in module:\n{source}\nitems: {items}"
 
     def getitems(self, source: Union[str, "os.PathLike[str]"]) -> List[Item]:
         """Return all test items collected from the module.
@@ -1426,10 +1427,7 @@ class Pytester:
             def handle_timeout() -> None:
                 __tracebackhide__ = True
 
-                timeout_message = (
-                    "{seconds} second timeout expired running:"
-                    " {command}".format(seconds=timeout, command=cmdargs)
-                )
+                timeout_message = f"{timeout} second timeout expired running: {cmdargs}"
 
                 popen.kill()
                 popen.wait()

src/_pytest/_code/__init__.py
@@ -10,6 +10,7 @@ from .code import TracebackEntry
 from .source import getrawcode
 from .source import Source
 
+
 __all__ = [
     "Code",
     "ExceptionInfo",

src/_pytest/_io/__init__.py
@@ -1,6 +1,7 @@
 from .terminalwriter import get_terminal_width
 from .terminalwriter import TerminalWriter
 
+
 __all__ = [
     "TerminalWriter",
     "get_terminal_width",

src/_pytest/assertion/__init__.py
@@ -16,6 +16,7 @@ from _pytest.config import hookimpl
 from _pytest.config.argparsing import Parser
 from _pytest.nodes import Item
 
+
 if TYPE_CHECKING:
     from _pytest.main import Session
 
@@ -129,7 +130,6 @@ def pytest_runtest_protocol(item: Item) -> Generator[None, object, object]:
     reporting via the pytest_assertrepr_compare hook.  This sets up this custom
     comparison for the test.
     """
-
     ihook = item.ihook
 
     def callbinrepr(op, left: object, right: object) -> Optional[str]:

src/_pytest/mark/__init__.py
@@ -24,6 +24,7 @@ from _pytest.config import UsageError
 from _pytest.config.argparsing import Parser
 from _pytest.stash import StashKey
 
+
 if TYPE_CHECKING:
     from _pytest.nodes import Item
 
@@ -268,8 +269,8 @@ def pytest_configure(config: Config) -> None:
 
     if empty_parameterset not in ("skip", "xfail", "fail_at_collect", None, ""):
         raise UsageError(
-            "{!s} must be one of skip, xfail or fail_at_collect"
-            " but it is {!r}".format(EMPTY_PARAMETERSET_OPTION, empty_parameterset)
+            f"{EMPTY_PARAMETERSET_OPTION!s} must be one of skip, xfail or fail_at_collect"
+            f" but it is {empty_parameterset!r}"
         )
 
 

commit cbd5a88ab4ede59f0ba8427f7056b651dc63eacf

doc: fix broken code blocks
Caused by 4588653b2497ed25976b7aaff225b889fb476756.

The issue was fixed in https://github.com/astral-sh/ruff/issues/11577,
so won't trigger again.

Fix #12437.

src/_pytest/capture.py
@@ -983,6 +983,7 @@ def capsys(request: SubRequest) -> Generator[CaptureFixture[str], None, None]:
     Returns an instance of :class:`CaptureFixture[str] <pytest.CaptureFixture>`.
 
     Example:
+
     .. code-block:: python
 
         def test_output(capsys):
@@ -1010,6 +1011,7 @@ def capsysbinary(request: SubRequest) -> Generator[CaptureFixture[bytes], None, 
     Returns an instance of :class:`CaptureFixture[bytes] <pytest.CaptureFixture>`.
 
     Example:
+
     .. code-block:: python
 
         def test_output(capsysbinary):
@@ -1037,6 +1039,7 @@ def capfd(request: SubRequest) -> Generator[CaptureFixture[str], None, None]:
     Returns an instance of :class:`CaptureFixture[str] <pytest.CaptureFixture>`.
 
     Example:
+
     .. code-block:: python
 
         def test_system_echo(capfd):
@@ -1064,6 +1067,7 @@ def capfdbinary(request: SubRequest) -> Generator[CaptureFixture[bytes], None, N
     Returns an instance of :class:`CaptureFixture[bytes] <pytest.CaptureFixture>`.
 
     Example:
+
     .. code-block:: python
 
         def test_system_echo(capfdbinary):

src/_pytest/config/__init__.py
@@ -1735,6 +1735,7 @@ class Config:
         can be used to explicitly use the global verbosity level.
 
         Example:
+
         .. code-block:: ini
 
             # content of pytest.ini

src/_pytest/monkeypatch.py
@@ -142,6 +142,7 @@ class MonkeyPatch:
         which undoes any patching done inside the ``with`` block upon exit.
 
         Example:
+
         .. code-block:: python
 
             import functools

src/_pytest/pytester.py
@@ -805,6 +805,7 @@ class Pytester:
             The first created file.
 
         Examples:
+
         .. code-block:: python
 
             pytester.makefile(".txt", "line1", "line2")
@@ -858,6 +859,7 @@ class Pytester:
         existing files.
 
         Examples:
+
         .. code-block:: python
 
             def test_something(pytester):
@@ -877,6 +879,7 @@ class Pytester:
         existing files.
 
         Examples:
+
         .. code-block:: python
 
             def test_something(pytester):