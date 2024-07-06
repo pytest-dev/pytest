3fc478ee392c1f689e9d4ba6724fed4d9ab539bc
FeRD (Frank Dana) <ferdnyc@gmail.com>
2024-07-06 04:04:05 -0400

    Docs: Fix custom styling, restore bullets
    - Move the custom CSS to a dedicated `pytest.css` file in `_static/`,
      and reference it from the configuraton's `html_css_files` list.
    - Update the list-customization styling to only affect `.sidebar-scroll`
      (the navigation sidebar), so that bullets are visible in the rest
      of the documentation.
    - Remove the list indent from the navigation sidebar, but only at the
      top level, so that navigation is still nested with visible indents.

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -251,7 +251,7 @@ html_favicon = "img/favicon.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -274,7 +274,6 @@ html_sidebars = {
         "globaltoc.html",
         "links.html",
         "sidebar/scroll-end.html",
-        "style.html",
     ],
     "**": [
         "sidebar/brand.html",
@@ -284,9 +283,12 @@ html_sidebars = {
         "relations.html",
         "links.html",
         "sidebar/scroll-end.html",
-        "style.html",
     ],
 }
+
+html_css_files = [
+    "pytest.css",
+]
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
