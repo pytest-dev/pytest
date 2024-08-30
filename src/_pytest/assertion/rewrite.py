47ca377c50aa2141006376ba12152d2f1f1083d8
dongfangtianyu <7629022+dongfangtianyu@users.noreply.github.com>
2024-08-30 22:37:18 +0800

    Fix assertion rewriting with importlib mode (#12716)
    Fixes #12659
    
    (cherry picked from commit 9a444d113658be6ccb8dc9f57ed118a1ef17c94c)

--- src/_pytest/assertion/rewrite.py
+++ src/_pytest/assertion/rewrite.py
@@ -101,6 +101,16 @@ class AssertionRewritingHook(importlib.abc.MetaPathFinder, importlib.abc.Loader)
 
         # Type ignored because mypy is confused about the `self` binding here.
         spec = self._find_spec(name, path)  # type: ignore
+
+        if spec is None and path is not None:
+            # With --import-mode=importlib, PathFinder cannot find spec without modifying `sys.path`,
+            # causing inability to assert rewriting (#12659).
+            # At this point, try using the file path to find the module spec.
+            for _path_str in path:
+                spec = importlib.util.spec_from_file_location(name, _path_str)
+                if spec is not None:
+                    break
+
         if (
             # the import machinery could not find a file to import
             spec is None
