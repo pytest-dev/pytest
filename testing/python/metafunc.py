0cb2599d5b35cc9d6c4ded65ae1959039e5f30b6
Christopher Head <chead@chead.ca>
2024-09-23 18:19:01 -0700

    fixup! Prohibit pytest.mark.usefixtures in pytest.param

--- testing/python/metafunc.py
+++ testing/python/metafunc.py
@@ -2114,3 +2114,10 @@ class TestMarkersWithParametrization:
                 "*= 6 passed in *",
             ]
         )
+
+    def test_param_usefixtures_rejected(self, pytester: Pytester) -> None:
+        """Verify that passing usefixtures to pytest.param fails loudly rather than silently."""
+        pytest.raises(
+            ValueError,
+            lambda: pytest.param("x", [1], marks=pytest.mark.usefixtures("foo")),
+        )
