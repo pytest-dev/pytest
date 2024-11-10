c8f095b70701d4a5df1048cb0cb282ff3ee54097
David Peled <davidp.info1@gmail.com>
2024-11-10 14:26:06 +0200

    fix comment

--- src/_pytest/main.py
+++ src/_pytest/main.py
@@ -12,6 +12,7 @@ import os
 from pathlib import Path
 import sys
 from typing import AbstractSet
+from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import final
@@ -365,6 +366,20 @@ def pytest_runtestloop(session: Session) -> bool:
     return True
 
 
+def _decode_toml_file(toml: Path) -> dict[str, Any] | None:
+    """Attempt to decode a toml file into a dict, returning None if fails."""
+    if sys.version_info >= (3, 11):
+        import tomllib
+    else:
+        import tomli as tomllib
+
+    try:
+        toml_text = toml.read_text(encoding="utf-8")
+        return tomllib.loads(toml_text)
+    except tomllib.TOMLDecodeError:
+        return None
+
+
 def _in_build(path: Path) -> bool:
     """Attempt to detect if ``path`` is the root of a buildsystem's artifacts
     by checking known dirnames patterns, and the presence of configuration in
@@ -374,9 +389,21 @@ def _in_build(path: Path) -> bool:
         return False
 
     if any(fnmatch_ex(pat, path) for pat in ("build", "dist")):
-        indicators = ("setup.py", "setup.cfg", "pyproject.toml")
-        if any((path.parent / f).is_file() for f in indicators):
-            return True
+        setup_cfg = path.parent / "setup.cfg"
+        if (setup_cfg).is_file():
+            setup_py = path.parent / "setup.py"
+            if setup_py.is_file():
+                return True
+
+            pyproject_toml = path.parent / "pyproject.toml"
+            if pyproject_toml.is_file():
+                config = _decode_toml_file(pyproject_toml)
+                if config:
+                    if any(
+                        "setuptools" in cfg
+                        for cfg in config.get("build-system", {}).get("requires", {})
+                    ):
+                        return True
 
     return False
 
