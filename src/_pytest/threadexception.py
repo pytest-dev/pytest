c24c03e852ce3969bf362ce1f25fcca53f277df8
Thomas Grainger <tagrain@gmail.com>
2024-12-01 08:16:09 +0000

    join straggling threads for 1 second before removing excepthook

--- src/_pytest/threadexception.py
+++ src/_pytest/threadexception.py
@@ -5,6 +5,7 @@ from collections.abc import Callable
 import functools
 import sys
 import threading
+import time
 import traceback
 from typing import NamedTuple
 from typing import TYPE_CHECKING
@@ -22,6 +23,22 @@ if TYPE_CHECKING:
 
 if sys.version_info < (3, 11):
     from exceptiongroup import ExceptionGroup
+
+
+def join_threads() -> None:
+    start = time.monotonic()
+    current_thread = threading.current_thread()
+    # This function is executed right at the end of the pytest run, just
+    # before we return an exit code, which is where the interpreter joins
+    # any remaining non-daemonic threads anyway, so it's ok to join all the
+    # threads. However there might be threads that depend on some shutdown
+    # signal that happens after pytest finishes, so we want to limit the
+    # join time somewhat. A one second timeout seems reasonable.
+    timeout = 1
+    for thread in threading.enumerate():
+        if thread is not current_thread and not thread.daemon:
+            # TODO: raise an error/warning if there's dangling threads.
+            thread.join(timeout - (time.monotonic() - start))
 
 
 class ThreadExceptionMeta(NamedTuple):
@@ -77,6 +94,7 @@ def cleanup(
 ) -> None:
     try:
         try:
+            join_threads()
             collect_thread_exception(config)
         finally:
             threading.excepthook = prev_hook
