pytest-dev/pytest

commit 9a444d113658be6ccb8dc9f57ed118a1ef17c94c

Fix assertion rewriting with importlib mode (#12716)
Fixes #12659

src/_pytest/pathlib.py
@@ -621,7 +621,9 @@ def _import_module_using_spec(
     # Checking with sys.meta_path first in case one of its hooks can import this module,
     # such as our own assertion-rewrite hook.
     for meta_importer in sys.meta_path:
-        spec = meta_importer.find_spec(module_name, [str(module_location)])
+        spec = meta_importer.find_spec(
+            module_name, [str(module_location), str(module_path)]
+        )
         if spec_matches_module_path(spec, module_path):
             break
     else:

testing/test_pathlib.py
@@ -1372,6 +1372,42 @@ class TestNamespacePackages:
         )
         assert mod is mod2
 
+    def test_ns_multiple_levels_import_rewrite_assertions(
+        self,
+        tmp_path: Path,
+        monkeypatch: MonkeyPatch,
+        pytester: Pytester,
+    ) -> None:
+        """Check assert rewriting with `--import-mode=importlib` (#12659)."""
+        self.setup_directories(tmp_path, monkeypatch, pytester)
+        code = dedent("""
+        def test():
+            assert "four lights" == "five lights"
+        """)
+
+        # A case is in a subdirectory with an `__init__.py` file.
+        test_py = tmp_path / "src/dist2/com/company/calc/algo/test_demo.py"
+        test_py.write_text(code, encoding="UTF-8")
+
+        pkg_root, module_name = resolve_pkg_root_and_module_name(
+            test_py, consider_namespace_packages=True
+        )
+        assert (pkg_root, module_name) == (
+            tmp_path / "src/dist2",
+            "com.company.calc.algo.test_demo",
+        )
+
+        result = pytester.runpytest("--import-mode=importlib", test_py)
+
+        result.stdout.fnmatch_lines(
+            [
+                "E       AssertionError: assert 'four lights' == 'five lights'",
+                "E         *",
+                "E         - five lights*",
+                "E         + four lights",
+            ]
+        )
+
     @pytest.mark.parametrize("import_mode", ["prepend", "append", "importlib"])
     def test_incorrect_namespace_package(
         self,

AUTHORS
@@ -412,6 +412,7 @@ Ted Xiao
 Terje Runde
 Thomas Grainger
 Thomas Hisch
+Tianyu Dongfang
 Tim Hoffmann
 Tim Strazny
 TJ Bruno

src/_pytest/assertion/rewrite.py
@@ -101,6 +101,16 @@ class AssertionRewritingHook(importlib.abc.MetaPathFinder, importlib.abc.Loader)
 
         # Type ignored because mypy is confused about the `self` binding here.
         spec = self._find_spec(name, path)  # type: ignore
+
+        if spec is None and path is not None:
+            # With --import-mode=importlib, PathFinder cannot find spec without modifying `sys.path`,
+            # causing inability to assert rewriting (#12659).
+            # At this point, try using the file path to find the module spec.
+            for _path_str in path:
+                spec = importlib.util.spec_from_file_location(name, _path_str)
+                if spec is not None:
+                    break
+
         if (
             # the import machinery could not find a file to import
             spec is None

commit 28e1e25a6782513db8a2963bd5ed5a9d66682f86

Fix `ImportError` crash when using `--import-mode=importlib` (#13053)
Regression in #12716  

In short: `PathFinder.find_spec` received the argument `/cow/moo` but loaded `/cow/moo/moo.py` instead.  

**Trigger conditions:**  
1. `/cow/moo/moo.py` exists (a file and directory with the same name).  
2. `/cow/moo/test_moo.py` exists (test case resides in the directory).  

When pytest loads test files in `importlib` mode, it continues recursive loading upward:  
- When loading `cow.moo`, it should return a namespace but unexpectedly returns a module.  
- When loading `cow.moo.moo`, it should return a module but unexpectedly returns a namespace.  

**Complete example:** [[GitHub repository](https://github.com/dongfangtianyu/pytest_importlib_issue)](https://github.com/dongfangtianyu/pytest_importlib_issue)  
- `main.py`: Reproduces the error.  
- `debug.py`: Demonstrates the behavior of `PathFinder.find_spec`.  

**Context:**
https://github.com/pytest-dev/pytest/issues/12592#issuecomment-2512039696
https://github.com/pytest-dev/pytest/issues/12592#issuecomment-2515100428


---------

Co-authored-by: Bruno Oliveira <bruno@soliv.dev>

src/_pytest/pathlib.py
@@ -694,9 +694,16 @@ def _import_module_using_spec(
     # Checking with sys.meta_path first in case one of its hooks can import this module,
     # such as our own assertion-rewrite hook.
     for meta_importer in sys.meta_path:
-        spec = meta_importer.find_spec(
-            module_name, [str(module_location), str(module_path)]
-        )
+        module_name_of_meta = getattr(meta_importer.__class__, "__module__", "")
+        if module_name_of_meta == "_pytest.assertion.rewrite" and module_path.is_file():
+            # Import modules in subdirectories by module_path
+            # to ensure assertion rewrites are not missed (#12659).
+            find_spec_path = [str(module_location), str(module_path)]
+        else:
+            find_spec_path = [str(module_location)]
+
+        spec = meta_importer.find_spec(module_name, find_spec_path)
+
         if spec_matches_module_path(spec, module_path):
             break
     else:

testing/test_pathlib.py
@@ -1498,6 +1498,34 @@ class TestNamespacePackages:
             ]
         )
 
+    def test_ns_multiple_levels_import_error(
+        self,
+        tmp_path: Path,
+        pytester: Pytester,
+    ) -> None:
+        # Trigger condition 1: ns and file with the same name
+        file = pytester.path / "cow/moo/moo.py"
+        file.parent.mkdir(parents=True)
+        file.write_text("data=123", encoding="utf-8")
+
+        # Trigger condition 2: tests are located in ns
+        tests = pytester.path / "cow/moo/test_moo.py"
+
+        tests.write_text(
+            dedent(
+                """
+            from cow.moo.moo import data
+
+            def test_moo():
+                print(data)
+            """
+            ),
+            encoding="utf-8",
+        )
+
+        result = pytester.runpytest("--import-mode=importlib")
+        assert result.ret == ExitCode.OK
+
     @pytest.mark.parametrize("import_mode", ["prepend", "append", "importlib"])
     def test_incorrect_namespace_package(
         self,