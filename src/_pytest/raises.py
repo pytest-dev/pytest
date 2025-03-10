f55fa8ea7feee3ef82dd5fed0992de481645090d
jakkdl <h6+github@pm.me>
2025-03-10 16:48:26 +0100

    fix rtd

--- src/_pytest/raises.py
+++ src/_pytest/raises.py
@@ -550,8 +550,10 @@ class RaisesExc(AbstractRaises[BaseExcT_co_default]):
 
         The type is checked with :func:`isinstance`, and does not need to be an exact match.
         If that is wanted you can use the ``check`` parameter.
+
     :kwparam str | Pattern[str] match
         A regex to match.
+
     :kwparam Callable[[BaseException], bool] check
         If specified, a callable that will be called with the exception as a parameter
         after checking the type and the match regex if specified.
