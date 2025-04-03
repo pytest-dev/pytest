465a9b220be59b0eb8384f769ab83f912418a283
Bruno Oliveira <bruno@soliv.dev>
2025-04-03 16:13:08 -0300

    Update src/_pytest/python_api.py

--- src/_pytest/python_api.py
+++ src/_pytest/python_api.py
@@ -678,7 +678,10 @@ def approx(expected, rel=None, abs=None, nan_ok: bool = False) -> ApproxBase:
         from the
         `re_assert package <https://github.com/asottile/re-assert>`_.
 
-        Note that unlike built-in equality, this function considers
+
+    .. note::
+    
+        Unlike built-in equality, this function considers
         booleans unequal to numeric zero or one. For example::
 
            >>> 1 == approx(True)
