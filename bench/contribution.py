d949e6b1ec4c302f85beedff82de0bae1687f642
DaveMiranda15 <davemiranda1983@yahoo.com>
2025-05-16 21:56:48 -0700

    Adding additional contribution.

---
+++ bench/contribution.py
@@ -0,0 +1,9 @@
+# My contribution to the project
+#Checking in to the project
+
+fname = "Dave"
+lname = "Miranda"   
+
+fullname = fname + " " + lname
+print("Hello, my name is " + fullname + " a CIAT Student")
+
