ae7346114d65e3a2c0ccd77af8a0ca00012cd91d
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2025-06-24 22:17:21 +0530

    Add `int` and `float`  variants to Parser.addini (#13560)
    Fixes #13559.
    
    ---------
    
    Co-authored-by: Bruno Oliveira <bruno@soliv.dev>

--- src/_pytest/config/argparsing.py
+++ src/_pytest/config/argparsing.py
@@ -174,7 +174,9 @@ class Parser:
         self,
         name: str,
         help: str,
-        type: Literal["string", "paths", "pathlist", "args", "linelist", "bool"]
+        type: Literal[
+            "string", "paths", "pathlist", "args", "linelist", "bool", "int", "float"
+        ]
         | None = None,
         default: Any = NOT_SET,
     ) -> None:
