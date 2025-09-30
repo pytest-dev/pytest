082a27e30153a840c467f9ade194602e6882d157
Ran Benita <ran@unusedvar.com>
2025-09-30 10:12:06 +0300

    compat: move `running_on_ci()` to compat
    Currently defined in `_pytest.assertion.util`, but then imported from
    `_pytest.terminal`, so makes sense to move it to the general utils
    place.

--- src/_pytest/compat.py
+++ src/_pytest/compat.py
@@ -304,3 +304,9 @@ class CallableBool:
 
     def __call__(self) -> bool:
         return self._value
+
+
+def running_on_ci() -> bool:
+    """Check if we're currently running on a CI system."""
+    env_vars = ["CI", "BUILD_NUMBER"]
+    return any(var in os.environ for var in env_vars)
