e26c0ae688a77132d13c523009ba07d17c7f2ca4
Omri Golan <52385317+gomri15@users.noreply.github.com>
2025-10-28 20:04:07 +0200

    fix #13537: Add support for ExceptionGroup with only Skipped exceptions in teardown (#13741)

--- src/_pytest/reports.py
+++ src/_pytest/reports.py
@@ -9,6 +9,7 @@ import dataclasses
 from io import StringIO
 import os
 from pprint import pprint
+import sys
 from typing import Any
 from typing import cast
 from typing import final
@@ -33,6 +34,10 @@ from _pytest.nodes import Collector
 from _pytest.nodes import Item
 from _pytest.outcomes import fail
 from _pytest.outcomes import skip
+
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
 
 
 if TYPE_CHECKING:
@@ -251,6 +256,52 @@ def _report_unserialization_failure(
     raise RuntimeError(stream.getvalue())
 
 
+def _format_failed_longrepr(
+    item: Item, call: CallInfo[None], excinfo: ExceptionInfo[BaseException]
+):
+    if call.when == "call":
+        longrepr = item.repr_failure(excinfo)
+    else:
+        # Exception in setup or teardown.
+        longrepr = item._repr_failure_py(
+            excinfo, style=item.config.getoption("tbstyle", "auto")
+        )
+    return longrepr
+
+
+def _format_exception_group_all_skipped_longrepr(
+    item: Item,
+    excinfo: ExceptionInfo[BaseExceptionGroup[BaseException | BaseExceptionGroup]],
+) -> tuple[str, int, str]:
+    r = excinfo._getreprcrash()
+    assert r is not None, (
+        "There should always be a traceback entry for skipping a test."
+    )
+    if all(
+        getattr(skip, "_use_item_location", False) for skip in excinfo.value.exceptions
+    ):
+        path, line = item.reportinfo()[:2]
+        assert line is not None
+        loc = (os.fspath(path), line + 1)
+        default_msg = "skipped"
+    else:
+        loc = (str(r.path), r.lineno)
+        default_msg = r.message
+
+    # Get all unique skip messages.
+    msgs: list[str] = []
+    for exception in excinfo.value.exceptions:
+        m = getattr(exception, "msg", None) or (
+            exception.args[0] if exception.args else None
+        )
+        if m and m not in msgs:
+            msgs.append(m)
+
+    reason = "; ".join(msgs) if msgs else default_msg
+    longrepr = (*loc, reason)
+    return longrepr
+
+
 @final
 class TestReport(BaseReport):
     """Basic test report object (also used for setup and teardown calls if
@@ -368,17 +419,24 @@ class TestReport(BaseReport):
                 if excinfo.value._use_item_location:
                     path, line = item.reportinfo()[:2]
                     assert line is not None
-                    longrepr = os.fspath(path), line + 1, r.message
+                    longrepr = (os.fspath(path), line + 1, r.message)
                 else:
                     longrepr = (str(r.path), r.lineno, r.message)
+            elif isinstance(excinfo.value, BaseExceptionGroup) and (
+                excinfo.value.split(skip.Exception)[1] is None
+            ):
+                # All exceptions in the group are skip exceptions.
+                outcome = "skipped"
+                excinfo = cast(
+                    ExceptionInfo[
+                        BaseExceptionGroup[BaseException | BaseExceptionGroup]
+                    ],
+                    excinfo,
+                )
+                longrepr = _format_exception_group_all_skipped_longrepr(item, excinfo)
             else:
                 outcome = "failed"
-                if call.when == "call":
-                    longrepr = item.repr_failure(excinfo)
-                else:  # exception in setup or teardown
-                    longrepr = item._repr_failure_py(
-                        excinfo, style=item.config.getoption("tbstyle", "auto")
-                    )
+                longrepr = _format_failed_longrepr(item, call, excinfo)
         for rwhen, key, content in item._report_sections:
             sections.append((f"Captured {key} {rwhen}", content))
         return cls(
