4b1aa0613352aa4a637db2d9ada4848a5755678d
Ran Benita <ran@unusedvar.com>
2025-11-02 14:17:09 +0200

    config: drop caching from `DropShorterLongHelpFormatter`
    Unlike what the comment says, it is not called multiple times when
    `--help` is used (it is not used at all when not).

--- src/_pytest/config/argparsing.py
+++ src/_pytest/config/argparsing.py
@@ -409,7 +409,6 @@ class DropShorterLongHelpFormatter(argparse.HelpFormatter):
 
     - Collapse **long** options that are the same except for extra hyphens.
     - Shortcut if there are only two options and one of them is a short one.
-    - Cache result on the action object as this is called at least 2 times.
     """
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -422,13 +421,9 @@ class DropShorterLongHelpFormatter(argparse.HelpFormatter):
         orgstr = super()._format_action_invocation(action)
         if orgstr and orgstr[0] != "-":  # only optional arguments
             return orgstr
-        res: str | None = getattr(action, "_formatted_action_invocation", None)
-        if res:
-            return res
         options = orgstr.split(", ")
         if len(options) == 2 and (len(options[0]) == 2 or len(options[1]) == 2):
             # a shortcut for '-h, --help' or '--abc', '-a'
-            action._formatted_action_invocation = orgstr  # type: ignore
             return orgstr
         return_list = []
         short_long: dict[str, str] = {}
@@ -451,9 +446,7 @@ class DropShorterLongHelpFormatter(argparse.HelpFormatter):
                 return_list.append(option)
             if option[2:] == short_long.get(option.replace("-", "")):
                 return_list.append(option.replace(" ", "=", 1))
-        formatted_action_invocation = ", ".join(return_list)
-        action._formatted_action_invocation = formatted_action_invocation  # type: ignore
-        return formatted_action_invocation
+        return ", ".join(return_list)
 
     def _split_lines(self, text, width):
         """Wrap lines after splitting on original newlines.
