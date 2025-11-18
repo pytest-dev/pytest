24dd541821363c1f6169b4919fd5a6a5ed4ccede
solaluset <60041069+solaluset@users.noreply.github.com>
2025-11-18 05:29:41 +0200

    Fix source IndexError test

--- testing/code/test_source.py
+++ testing/code/test_source.py
@@ -663,9 +663,10 @@ def test_patched_compile() -> None:
 
     # fmt: off
     def patched_compile2(_, *args, **kwargs):
-    # first line of this function must not start with spaces
-    # LINES must be equal to number of lines of this function
-        LINES = 4
+
+        # first line of this function (the one above this one) must be empty
+        # LINES must be equal or higher than number of lines of this function
+        LINES = 99
         return compile("\ndef a():\n" + "\n" * LINES + "    pass", *args, **kwargs)
     # fmt: on
 
