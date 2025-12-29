f304c55edb08972133d7f4c5d3b0cdba1394ed00
Bubble-Interface <interfacemichael@gmail.com>
2025-12-29 14:16:10 +0300

    Show better error when blocking conftest files via `-p` (#14018)
    Show a clear message when `-p` is used for a  `conftest.py` file, instead of raising an internal assertion error.
    
    Fixes #13634
    
    (cherry picked from commit 004a96740fb990c7d6dc39bd305b08852635ec5d)

--- testing/test_config.py
+++ testing/test_config.py
@@ -2460,6 +2460,10 @@ def test_config_does_not_load_blocked_plugin_from_args(pytester: Pytester) -> No
     result.stderr.fnmatch_lines(["*: error: unrecognized arguments: -s"])
     assert result.ret == ExitCode.USAGE_ERROR
 
+    result = pytester.runpytest(str(p), "-p no:/path/to/conftest.py", "-s")
+    result.stderr.fnmatch_lines(["ERROR:*Blocking conftest files*"])
+    assert result.ret == ExitCode.USAGE_ERROR
+
 
 def test_invocation_args(pytester: Pytester) -> None:
     """Ensure that Config.invocation_* arguments are correctly defined"""
