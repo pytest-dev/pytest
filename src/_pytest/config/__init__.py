pytest-dev/pytest

commit 661682c25a9ff844a66ebd9b95d9925b8c4b65c8

Handle --version eagerly to avoid loading the entire infrastructure (#13575)
Currently, handling `--version` in `pytest_cmdline_main` requires loading the entire infrastructure, which can be slow depending on the installed plugins.

This change introduces a marginal behavioral difference, though it should not cause any issues in practice.

Fixes #13574

src/_pytest/config/__init__.py
@@ -149,11 +149,17 @@ def main(
 
     :returns: An exit code.
     """
+    # Handle a single `--version` argument early to avoid starting up the entire pytest infrastructure.
+    new_args = sys.argv[1:] if args is None else args
+    if isinstance(new_args, Sequence) and new_args.count("--version") == 1:
+        sys.stdout.write(f"pytest {__version__}\n")
+        return ExitCode.OK
+
     old_pytest_version = os.environ.get("PYTEST_VERSION")
     try:
         os.environ["PYTEST_VERSION"] = __version__
         try:
-            config = _prepareconfig(args, plugins)
+            config = _prepareconfig(new_args, plugins)
         except ConftestImportFailure as e:
             exc_info = ExceptionInfo.from_exception(e.cause)
             tw = TerminalWriter(sys.stderr)
@@ -317,12 +323,10 @@ def get_plugin_manager() -> PytestPluginManager:
 
 
 def _prepareconfig(
-    args: list[str] | os.PathLike[str] | None = None,
+    args: list[str] | os.PathLike[str],
     plugins: Sequence[str | _PluggyPlugin] | None = None,
 ) -> Config:
-    if args is None:
-        args = sys.argv[1:]
-    elif isinstance(args, os.PathLike):
+    if isinstance(args, os.PathLike):
         args = [os.fspath(args)]
     elif not isinstance(args, list):
         msg = (  # type:ignore[unreachable]
@@ -1145,13 +1149,15 @@ class Config:
         try:
             self.parse(args)
         except UsageError:
-            # Handle --version and --help here in a minimal fashion.
+            # Handle `--version --version` and `--help` here in a minimal fashion.
             # This gets done via helpconfig normally, but its
             # pytest_cmdline_main is not called in case of errors.
             if getattr(self.option, "version", False) or "--version" in args:
-                from _pytest.helpconfig import showversion
+                from _pytest.helpconfig import show_version_verbose
 
-                showversion(self)
+                # Note that `--version` (single argument) is handled early by `Config.main()`, so the only
+                # way we are reaching this point is via `--version --version`.
+                show_version_verbose(self)
             elif (
                 getattr(self.option, "help", False) or "--help" in args or "-h" in args
             ):

testing/test_helpconfig.py
@@ -10,21 +10,21 @@ def test_version_verbose(pytester: Pytester, pytestconfig, monkeypatch) -> None:
     monkeypatch.delenv("PYTEST_DISABLE_PLUGIN_AUTOLOAD")
     monkeypatch.delenv("PYTEST_PLUGINS", raising=False)
     result = pytester.runpytest("--version", "--version")
-    assert result.ret == 0
+    assert result.ret == ExitCode.OK
     result.stdout.fnmatch_lines([f"*pytest*{pytest.__version__}*imported from*"])
     if pytestconfig.pluginmanager.list_plugin_distinfo():
         result.stdout.fnmatch_lines(["*registered third-party plugins:", "*at*"])
 
 
-def test_version_less_verbose(pytester: Pytester, pytestconfig, monkeypatch) -> None:
-    monkeypatch.delenv("PYTEST_DISABLE_PLUGIN_AUTOLOAD")
-    monkeypatch.delenv("PYTEST_PLUGINS", raising=False)
-    result = pytester.runpytest("--version")
-    assert result.ret == 0
-    result.stdout.fnmatch_lines([f"pytest {pytest.__version__}"])
+def test_version_less_verbose(pytester: Pytester) -> None:
+    """Single ``--version`` parameter should display only the pytest version, without loading plugins (#13574)."""
+    pytester.makeconftest("print('This should not be printed')")
+    result = pytester.runpytest_subprocess("--version")
+    assert result.ret == ExitCode.OK
+    assert result.stdout.str().strip() == f"pytest {pytest.__version__}"
 
 
-def test_versions():
+def test_versions() -> None:
     """Regression check for the public version attributes in pytest."""
     assert isinstance(pytest.__version__, str)
     assert isinstance(pytest.version_tuple, tuple)
@@ -32,7 +32,7 @@ def test_versions():
 
 def test_help(pytester: Pytester) -> None:
     result = pytester.runpytest("--help")
-    assert result.ret == 0
+    assert result.ret == ExitCode.OK
     result.stdout.fnmatch_lines(
         """
           -m MARKEXPR           Only run tests matching given mark expression. For
@@ -73,7 +73,7 @@ def test_empty_help_param(pytester: Pytester) -> None:
     """
     )
     result = pytester.runpytest("--help")
-    assert result.ret == 0
+    assert result.ret == ExitCode.OK
     lines = [
         "  required_plugins (args):",
         "                        Plugins that must be present for pytest to run*",
@@ -91,7 +91,7 @@ def test_hookvalidation_unknown(pytester: Pytester) -> None:
     """
     )
     result = pytester.runpytest()
-    assert result.ret != 0
+    assert result.ret != ExitCode.OK
     result.stdout.fnmatch_lines(["*unknown hook*pytest_hello*"])
 
 

src/_pytest/helpconfig.py
@@ -140,28 +140,28 @@ def pytest_cmdline_parse() -> Generator[None, Config, Config]:
     return config
 
 
-def showversion(config: Config) -> None:
-    if config.option.version > 1:
-        sys.stdout.write(
-            f"This is pytest version {pytest.__version__}, imported from {pytest.__file__}\n"
-        )
-        plugininfo = getpluginversioninfo(config)
-        if plugininfo:
-            for line in plugininfo:
-                sys.stdout.write(line + "\n")
-    else:
-        sys.stdout.write(f"pytest {pytest.__version__}\n")
+def show_version_verbose(config: Config) -> None:
+    """Show verbose pytest version installation, including plugins."""
+    sys.stdout.write(
+        f"This is pytest version {pytest.__version__}, imported from {pytest.__file__}\n"
+    )
+    plugininfo = getpluginversioninfo(config)
+    if plugininfo:
+        for line in plugininfo:
+            sys.stdout.write(line + "\n")
 
 
 def pytest_cmdline_main(config: Config) -> int | ExitCode | None:
-    if config.option.version > 0:
-        showversion(config)
-        return 0
+    # Note: a single `--version` argument is handled directly by `Config.main()` to avoid starting up the entire
+    # pytest infrastructure just to display the version (#13574).
+    if config.option.version > 1:
+        show_version_verbose(config)
+        return ExitCode.OK
     elif config.option.help:
         config._do_configure()
         showhelp(config)
         config._ensure_unconfigure()
-        return 0
+        return ExitCode.OK
     return None
 
 

testing/test_capture.py
@@ -868,7 +868,7 @@ def test_error_during_readouterr(pytester: Pytester) -> None:
         FDCapture.snap = bad_snap
     """
     )
-    result = pytester.runpytest_subprocess("-p", "pytest_xyz", "--version")
+    result = pytester.runpytest_subprocess("-p", "pytest_xyz")
     result.stderr.fnmatch_lines(
         ["*in bad_snap", "    raise Exception('boom')", "Exception: boom"]
     )

testing/test_config.py
@@ -612,20 +612,14 @@ class TestConfigCmdlineParsing:
         assert config.getini("custom") == "1"
 
     def test_absolute_win32_path(self, pytester: Pytester) -> None:
-        temp_ini_file = pytester.makefile(
-            ".ini",
-            custom="""
-            [pytest]
-            addopts = --version
-        """,
-        )
+        temp_ini_file = pytester.makeini("[pytest]")
         from os.path import normpath
 
         temp_ini_file_norm = normpath(str(temp_ini_file))
         ret = pytest.main(["-c", temp_ini_file_norm])
-        assert ret == ExitCode.OK
+        assert ret == ExitCode.NO_TESTS_COLLECTED
         ret = pytest.main(["--config-file", temp_ini_file_norm])
-        assert ret == ExitCode.OK
+        assert ret == ExitCode.NO_TESTS_COLLECTED
 
 
 class TestConfigAPI:
@@ -2121,7 +2115,7 @@ def test_help_and_version_after_argument_error(pytester: Pytester) -> None:
 
     result = pytester.runpytest("--version")
     result.stdout.fnmatch_lines([f"pytest {pytest.__version__}"])
-    assert result.ret == ExitCode.USAGE_ERROR
+    assert result.ret == ExitCode.OK
 
 
 def test_help_formatter_uses_py_get_terminal_width(monkeypatch: MonkeyPatch) -> None:

commit 0669db98e96669afc470ed16b6d3458bbab764ae

Fix `-V` to show version information
This regressed in #13575.

Fixes #14381.

src/_pytest/config/__init__.py
@@ -182,9 +182,12 @@ def main(
 
     :returns: An exit code.
     """
-    # Handle a single `--version` argument early to avoid starting up the entire pytest infrastructure.
+    # Handle a single `--version`/`-V` argument early to avoid starting up the entire pytest infrastructure.
     new_args = sys.argv[1:] if args is None else args
-    if isinstance(new_args, Sequence) and new_args.count("--version") == 1:
+    if (
+        isinstance(new_args, Sequence)
+        and (new_args.count("--version") + new_args.count("-V")) == 1
+    ):
         sys.stdout.write(f"pytest {__version__}\n")
         return ExitCode.OK
 

testing/test_helpconfig.py
@@ -16,10 +16,11 @@ def test_version_verbose(pytester: Pytester, pytestconfig, monkeypatch) -> None:
         result.stdout.fnmatch_lines(["*registered third-party plugins:", "*at*"])
 
 
-def test_version_less_verbose(pytester: Pytester) -> None:
-    """Single ``--version`` parameter should display only the pytest version, without loading plugins (#13574)."""
+@pytest.mark.parametrize("flag", ["--version", "-V"])
+def test_version_less_verbose(pytester: Pytester, flag: str) -> None:
+    """Single ``--version`` or ``-V`` should display only the pytest version, without loading plugins (#13574)."""
     pytester.makeconftest("print('This should not be printed')")
-    result = pytester.runpytest_subprocess("--version")
+    result = pytester.runpytest_subprocess(flag)
     assert result.ret == ExitCode.OK
     assert result.stdout.str().strip() == f"pytest {pytest.__version__}"
 