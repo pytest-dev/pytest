93874f90a9aebfefbd39b9da70106ccece029f82
axcelsyah <164727007+axcelsyah@users.noreply.github.com>
2026-07-03 14:49:11 +0700

    fix: correct docstring placement for setup()

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -282,15 +282,7 @@ if IS_RELEASE_ON_RTD:
 
 
 def setup(app: sphinx.application.Sphinx) -> None:
-    """Setup.
-
-    Args:
-        app: Description of app.
-
-    Returns:
-        Description of return value.
-
-    """
+    """Document setup."""
     app.add_crossref_type(
         "fixture",
         "fixture",
