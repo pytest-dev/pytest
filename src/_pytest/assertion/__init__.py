jbfcd/test

Add invalidation-mode option

src/_pytest/assertion/__init__.py
@@ -83,6 +83,7 @@ class AssertionState:
     def __init__(self, config: Config, mode) -> None:
         self.mode = mode
         self.trace = config.trace.root.get("assertion")
+        self.invalidation_mode = config.option.invalidationmode
         self.hook: Optional[rewrite.AssertionRewritingHook] = None
 
 

pytest-dev/pytest

src/_pytest/assertion/__init__.py
@@ -128,6 +128,7 @@ class AssertionState:
     def __init__(self, config: Config, mode) -> None:
         self.mode = mode
         self.trace = config.trace.root.get("assertion")
+        self.invalidation_mode = config.known_args_namespace.invalidationmode
         self.hook: rewrite.AssertionRewritingHook | None = None
 
 